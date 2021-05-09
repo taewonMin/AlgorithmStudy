071a4c7aac91315416d8320f753f77fea8cf0034
taewonMin <mtw9411@naver.com>
2021-05-09 15:50:55 +0900

    커밋과 푸쉬를 동시에

--- AlgorithmStudy/src/week1/mtw/Math1_1712.java
+++ AlgorithmStudy/src/week1/mtw/Math1_1712.java
@@ -3,5 +3,6 @@ package week1.mtw;
 public class Math1_1712 {
 	public static void main(String[] args) {
 		System.out.println("Ä¿¹ÔÇÏ°í Çª½ÃÇÏ±â");
+		System.out.println("Ä¿¹Ô°ú Çª½Ã¸¦ µ¿½Ã¿¡");
 	}
 }
