8dd5f9c6c861b96515e6baf94041e3e9b8c426a5
user <user@192.168.219.102>
2021-05-21 22:08:52 +0900

    가장큰수

--- AlgorithmStudy/src/week1/chw/LargestNumber.java
+++ AlgorithmStudy/src/week1/chw/LargestNumber.java
@@ -47,3 +47,59 @@ public class LargestNumber {
         System.out.println(answer);
 	}
 }
+
+
+
+
+
+
+
+
+
+// 프로그래머스
+//class Solution {
+//    public String solution(int[] numbers) {
+//        
+//		int[] multiNumbers = new int[numbers.length];
+//		String strNum = "";
+//        for(int number = 0; number < numbers.length; number++){
+//        	multiNumbers[number] = numbers[number];
+//            strNum += numbers[number] + "";
+//        }
+//        int totalLength = strNum.length();
+//        
+//        for(int number = 0; number < multiNumbers.length; number++){
+//            String strNo = multiNumbers[number] + "";
+//            int numberLength = totalLength - strNo.length();
+//            multiNumbers[number] = (int) (multiNumbers[number] * Math.pow(10, numberLength));
+//        }
+//        
+//        for(int i=0; i<multiNumbers.length; i++) {
+//			for(int j=i+1; j<multiNumbers.length; j++) {
+//				if(multiNumbers[i] < multiNumbers[j]) { //내림차순
+//					
+//					int tmp = multiNumbers[i];
+//					multiNumbers[i] = multiNumbers[j];
+//					multiNumbers[j] = tmp;
+//					
+//					int realTemp = numbers[i];
+//                    numbers[i] = numbers[j];
+//                    numbers[j] = realTemp;
+//				}else if(multiNumbers[i] == multiNumbers[j]){
+//					if(numbers[i] > numbers[j]) {
+//						int realTemp = numbers[i];
+//						numbers[i] = numbers[j];
+//						numbers[j] = realTemp;
+//					}
+//				}
+//			}
+//		}
+//        
+//        String answer = "";
+//        for(int result : numbers){
+//            answer+= result+"";
+//        }
+//        
+//        return answer;
+//    }
+//}
