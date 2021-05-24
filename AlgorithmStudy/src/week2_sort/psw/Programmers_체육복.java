4bd05272013f5c9b4b19651c106b4e816fcaaff7
IAN <IAN@DESKTOP-P83RIDI>
2021-05-24 10:01:09 +0900

    체육복 수정

--- AlgorithmStudy/src/week2_sort/psw/Programmers_체육복.java
+++ AlgorithmStudy/src/week2_sort/psw/Programmers_체육복.java
@@ -1,88 +1,97 @@
 package week2_sort.psw;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Programmers_체육복 {
 	public static void main(String[] args) {
 		int answer = 0;
 		int n = 5;
 		int[] lost = {2,4};
-		int[] reserve = {1,3,5};
+		int[] reserve = {3};
 
+		List<Student> studentList = new ArrayList<Student>();
+		
+		// 학생 객체 생성
 		for(int i=0;i<n;i++){
-			Student.v.add(new Student(i));
+			studentList.add(new Student(i));
 		}
-
-		//////////////////////////// 학생 객체 생성
-
+		// 누가 훔쳐갔으면 일단 수업 못감
 		for(int i=0;i<lost.length;i++){
-			Student s=(Student)Student.v.elementAt(lost[i]-1);
+			Student s = studentList.get(lost[i]-1);
 			s.stolen=true;	
 			s.goClass=false;
 		}
+		
+		// 여벌있는지 유무 셋팅
 		for(int i=0;i<reserve.length;i++){
-			Student s=(Student)Student.v.elementAt(reserve[i]-1);
+			Student s = studentList.get(reserve[i]-1);
 			s.reserve=true;	
 		}
-		///////////////////////// 도둑맞은 여부와 여벌 여부
 
-		for(int i=0;i<Student.v.size();i++){
-			Student s=(Student)Student.v.elementAt(i);
-			if(s.stolen&&s.reserve){
+		// 도둑맞은 사람이 여벌이 있을 경우 우선적으로 여벌을 없애고 수업 참여
+		for(int i=0;i< studentList.size();i++){
+			Student s= studentList.get(i);
+			if( s.stolen && s.reserve){
 				s.goClass=true;
 				s.reserve=false;
 			}
 		}
-		////////////////////도둑맞은 사람이 여벌이 있을 경우 여벌을 없애고 수업 고고싱
+		
+		// 분배의 시간
 		for(int i=0;i<lost.length;i++){
-			Student s1=(Student)Student.v.elementAt(lost[i]-1);
-			if(!s1.goClass&&s1.num==1){
-				Student s3=(Student)Student.v.elementAt(lost[i]);
-				if(s3.reserve){
-					s1.goClass=true;
-					s3.reserve=false;
+			Student curStu = studentList.get(lost[i]-1);
+			
+			// 첫번째 친구일때는 다음 학생의 여분만 가져옴
+			if(!curStu.goClass && curStu.num == 1){
+				Student nextStu = studentList.get(lost[i]);
+				if(nextStu.reserve){
+					curStu.goClass=true;
+					nextStu.reserve=false;
 				}
-			} else if(!s1.goClass&&(lost[i]-1>0&&lost[i]!=n)){
-				Student s2=(Student)Student.v.elementAt(lost[i]-2);
-				Student s3=(Student)Student.v.elementAt(lost[i]);	
-				if(s2.reserve){
-					s1.goClass=true;
-					s2.reserve=false;
-				} else if(s3.reserve){
-					s1.goClass=true;
-					s3.reserve=false;
+			
+			// 중간에 있는 친구는 앞,뒤 모두 여벌을 찾아야함
+			} else if(!curStu.goClass && (lost[i]-1 > 0 && lost[i] != n)){
+				Student preStu = studentList.get(lost[i]-2);
+				Student nextStu = studentList.get(lost[i]);	
+				if(preStu.reserve){
+					curStu.goClass=true;
+					preStu.reserve=false;
+				} else if(nextStu.reserve){
+					curStu.goClass=true;
+					nextStu.reserve=false;
 				}
-			} else if(!s1.goClass&&s1.num==n){
-				Student s2=(Student)Student.v.elementAt(lost[i]-2);
-				if(s2.reserve){
-					s1.goClass=true;
-					s2.reserve=false;
+				
+			//끝에 있는 친구는 이전 친구만 가져옴
+			} else if(!curStu.goClass && curStu.num == n){
+				Student preStu= studentList.get(lost[i]-2);
+				if(preStu.reserve){
+					curStu.goClass = true;
+					preStu.reserve = false;
 				}
 			}
 		}
 
-
-
-
-		///////////////////////////////////////// 개수 측정
-		for(int i=0;i<Student.v.size();i++){
-			Student s=(Student)Student.v.elementAt(i);
-			if(s.goClass){
+		// 개수 측정
+		for(int i=0;i<studentList.size();i++){
+			Student stu= studentList.get(i);
+			if(stu.goClass){
 				answer++;
 			}
 		}
-
-
+		
+		System.out.println(answer);
+		
 	}
 
 }
 
 class Student{
-	static Vector v=new Vector();
 	int num;
-	boolean reserve;
-	boolean stolen;
-	boolean goClass=true;
+	
+	boolean reserve = false;
+	boolean stolen = false;
+	boolean goClass = true;
 
 
 	public Student(int i) {
