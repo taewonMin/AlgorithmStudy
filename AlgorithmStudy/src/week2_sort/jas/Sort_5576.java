e6a7de9f01154ab3ed793d6546e58f554cb7c974
jo <jo@DESKTOP-08UT35O>
2021-05-26 21:10:33 +0900

    이게되나

--- AlgorithmStudy/src/week2_sort/jas/Sort_5576.java
+++ AlgorithmStudy/src/week2_sort/jas/Sort_5576.java
@@ -1,5 +1,33 @@
 package week2_sort.jas;
 
-public class Sort_5576 {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class Sort_5576 {
+	public static void main(String[] args) {
+		
+		Integer wArr[] = new Integer[10];
+		Integer kArr[] = new Integer[10];
+		
+		Scanner scan = new Scanner(System.in);
+		
+		for(int i=0; i<10; i++) {
+			wArr[i] = scan.nextInt();
+		}
+		for(int i=0; i<10; i++) {
+			kArr[i] = scan.nextInt();
+		}
+		
+		Arrays.sort(wArr, Collections.reverseOrder());
+		Arrays.sort(kArr, Collections.reverseOrder());
+		
+		int wSum = 0;
+		int kSum = 0;
+		for(int i=0; i<3; i++) {
+			wSum += wArr[i];
+			kSum += kArr[i];
+		}
+		System.out.println(wSum + " " + kSum);
+	}
 }
