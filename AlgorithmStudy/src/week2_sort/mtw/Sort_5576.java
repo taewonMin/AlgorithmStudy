8879cd1b3542218f8ec9e591be778c4c603026e8
taewonMin <mtw9411@naver.com>
2021-05-26 23:13:32 +0900

    5576 콘테스트

--- AlgorithmStudy/src/week2_sort/mtw/Sort_5576.java
+++ AlgorithmStudy/src/week2_sort/mtw/Sort_5576.java
@@ -1,5 +1,51 @@
 package week2_sort.mtw;
 
-public class Sort_5576 {
+import java.util.Scanner;
 
+// 콘테스트
+public class Sort_5576 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] wArr = new int[10];
+		int[] kArr = new int[10];
+		
+		for(int i=0; i<wArr.length; i++) {
+			wArr[i] = sc.nextInt();
+			
+			// 삽입정렬
+			int j = i;
+			while(j > 0 && wArr[j-1] < wArr[j]) {
+				int temp = wArr[j];
+				wArr[j] = wArr[j-1];
+				wArr[j-1] = temp;
+				j--;
+			}
+		}
+		
+		for(int i=0; i<kArr.length; i++) {
+			kArr[i] = sc.nextInt();
+			
+			// 삽입정렬
+			int j = i;
+			while(j > 0 && kArr[j-1] < kArr[j]) {
+				int temp = kArr[j];
+				kArr[j] = kArr[j-1];
+				kArr[j-1] = temp;
+				j--;
+			}
+		}
+
+		int wSum = 0;
+		int kSum = 0;
+		for(int i=0; i<3; i++) {
+			wSum += wArr[i];
+			kSum += kArr[i];
+		}
+		
+		System.out.println(wSum + " " + kSum);
+		
+		sc.close();
+	}
 }
