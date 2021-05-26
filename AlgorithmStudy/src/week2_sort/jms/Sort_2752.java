ce54491bc4c4607ad6ebc6ab0eef576350f852cb
jangminsu <jangminsu@localhost>
2021-05-26 20:59:25 +0900

    민수 2주

--- AlgorithmStudy/src/week2_sort/jms/Sort_2752.java
+++ AlgorithmStudy/src/week2_sort/jms/Sort_2752.java
@@ -1,5 +1,20 @@
 package week2_sort.jms;
 
-public class Sort_2752 {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class Sort_2752 {
+	public static void main(String[] args) {
+		List<Long> listOfNum = new ArrayList<Long>();
+		for (int i = 0; i < 3; i++) {
+			listOfNum.add((long)(Math.random()*1000000+1));
+		}
+		
+		listOfNum.sort(Comparator.naturalOrder());
+		
+		
+		System.out.println(listOfNum.toString());
+		
+	}
 }
--- AlgorithmStudy/src/week2_sort/jms/Sort_5576.java
+++ AlgorithmStudy/src/week2_sort/jms/Sort_5576.java
@@ -1,5 +1,54 @@
 package week2_sort.jms;
 
-public class Sort_5576 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+public class Sort_5576 {
+	public static void main(String[] args) {
+		List<Integer> listOfScore = new ArrayList<Integer>();
+		for (int i = 0; i < 20; i++) {
+			listOfScore.add((int)(Math.random()*100+1));
+		}
+		
+		Integer[] a = new Integer[10];
+		Integer[] b = new Integer[10];
+		int index = 0;
+		
+		for (int i = 0; i < 20; i++) {
+			if (i <10) {
+				a[i] = listOfScore.get(i);
+			
+			}else {
+				b[index] = listOfScore.get(i);
+				index++;
+				
+			}
+		}
+
+		Arrays.sort(a,Collections.reverseOrder());
+		Arrays.sort(b,Collections.reverseOrder());
+
+		int aScore = 0;
+		int bScore = 0;
+
+		
+		for (int i = 0; i < 3; i++) {
+			aScore += a[i];
+			bScore += b[i];
+		}
+		System.out.println("a 값"+Arrays.toString(a));
+		System.out.println("b 값"+Arrays.toString(b));
+		System.out.println("a 값="+aScore);
+		System.out.println("b 값="+bScore);
+		
+		
+		
+		
+		
+	}
+	
 }
+
