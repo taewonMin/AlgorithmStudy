b9f50cd5692b80546085f0876b84d11aba4d0669
IAN <IAN@DESKTOP-P83RIDI>
2021-05-30 22:26:28 +0900

    test

--- AlgorithmStudy/src/week3_sort/psw/Sort_13333.java
+++ AlgorithmStudy/src/week3_sort/psw/Sort_13333.java
@@ -1,5 +1,14 @@
 package week3_sort.psw;
 
-public class Sort_13333 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Sort_13333 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<Integer> list = new ArrayList<Integer>();
+		
+	}
 }
