464995deb5a705e03a7ff521d4b3a5492bf816b3
Ian <gramian7756@gmail.com>
2021-05-31 15:18:40 +0900

    백준

--- AlgorithmStudy/src/week3_sort/psw/Sort_13333.java
+++ AlgorithmStudy/src/week3_sort/psw/Sort_13333.java
@@ -3,11 +3,23 @@ package week3_sort.psw;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+// 테스트 통과 하지만... 실패... 
 public class Sort_13333 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int answer = 0;
 		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			list.add(sc.nextInt());
+		}
+		list.sort(null);
+		for (int i = 0; i < n; i++) {
+			int k = list.get(i);
+			if(k <= n-i) {
+				answer = k;
+			}
+		}
+		System.out.println(answer);
 	}
 }
--- AlgorithmStudy/src/week3_sort/psw/Sort_6996.java
+++ AlgorithmStudy/src/week3_sort/psw/Sort_6996.java
@@ -1,5 +1,52 @@
 package week3_sort.psw;
 
-public class Sort_6996 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Sort_6996 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < n; i++) {
+			sc = new Scanner(System.in);
+			String y = sc.nextLine();
+			String[] x = y.split(" ");
+			
+			String a = x[0];
+			String b = x[1];
+			
+			String answer = a + " & " + b + " are ";
+			answer += isAnagram(a, b);
+			list.add(answer);
+		}
+		
+		for (String string : list) {
+			System.out.println(string);
+		}
+		
+	}
+	
+	public static String isAnagram(String a, String b) {
+		List<String> list1 = new ArrayList<String>();
+		List<String> list2 = new ArrayList<String>();
+		for (int i = 0; i < a.length(); i++) {
+			list1.add(a.charAt(i)+"");
+		}
+		for (int i = 0; i < b.length(); i++) {
+			list2.add(b.charAt(i)+"");
+		}
+		list1.sort(null);
+		list2.sort(null);
+		
+		if(list1.size() != list2.size()) return "NOT anagrams.";
+		
+		for (int i = 0; i < list1.size(); i++) {
+			if (!list1.get(i).equals(list2.get(i))) {
+				return "NOT anagrams.";
+			}
+		}
+		return "anagrams.";
+	}
 }
