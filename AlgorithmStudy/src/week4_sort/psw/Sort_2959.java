0462b3c6505f2a712e6676f02ecdc7c2d8b1de21
Ian <gramian7756@gmail.com>
2021-06-06 19:28:49 +0900

    거북이 달리다

--- AlgorithmStudy/src/week4_sort/psw/Sort_2959.java
+++ AlgorithmStudy/src/week4_sort/psw/Sort_2959.java
@@ -1,5 +1,25 @@
 package week4_sort.psw;
 
-public class Sort_2959 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class Sort_2959 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 4; i++) {
+			list.add(sc.nextInt());
+		}
+		sc.close();
+		list.sort(Collections.reverseOrder());
+		int a,b,c,d;
+		a = list.get(0);
+		b = list.get(1);
+		c = list.get(2);
+		d = list.get(3);
+		int total = Math.min(a, b)*Math.min(c, d);
+		System.out.println(total);
+	}
 }
