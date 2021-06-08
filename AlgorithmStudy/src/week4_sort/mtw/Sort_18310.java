fe75326de77f2fa1be75b71469d6a2ed5be374f4
taewonMin <mtw9411@naver.com>
2021-06-09 03:24:16 +0900

    18310 안테나

--- AlgorithmStudy/src/week4_sort/mtw/Sort_18310.java
+++ AlgorithmStudy/src/week4_sort/mtw/Sort_18310.java
@@ -1,5 +1,26 @@
 package week4_sort.mtw;
 
-public class Sort_18310 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+// 안테나
+public class Sort_18310 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		sc.nextLine();
+		
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		int mid = (arr[arr.length-1] + arr[0]) / 2;
+		
+		System.out.println(mid);
+		
+		sc.close();
+	}
 }
