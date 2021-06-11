ee70aa96ac4c7678102ae7cd834c7daab64bf099
Ian <gramian7756@gmail.com>
2021-06-11 13:37:56 +0900

    백준

--- AlgorithmStudy/src/week5_sort/psw/Sort_15819.java
+++ AlgorithmStudy/src/week5_sort/psw/Sort_15819.java
@@ -1,5 +1,37 @@
 package week5_sort.psw;
 
-public class Sort_15819 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Sort_15819 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		for (int i = 0; i < count; i++) {
+			List<Integer> list = new ArrayList<Integer>();
+			int length = sc.nextInt();
+			for (int j = 0; j < length; j++) {
+				list.add(sc.nextInt());
+			}
+			
+			list.sort((Integer a, Integer b) -> -a.compareTo(b));
+			int[] answerList = new int[list.size()];
+			for (int j = 0; j < answerList.length; j++) {
+				int index = j%2 == 0 ? j/2 : answerList.length-((j/2)+1);
+				answerList[index] = list.remove(list.size()-1);
+			}
+			
+			int answer = Math.abs(answerList[0]-answerList[answerList.length-1]);
+			for (int j = 0; j < answerList.length-1; j++) {
+				int temp = Math.abs(answerList[j]-answerList[j+1]);
+				if(answer < temp) answer = temp;
+			}
+			
+			System.out.println(answer);
+		}
+		
+		sc.close();
+	}
 }
