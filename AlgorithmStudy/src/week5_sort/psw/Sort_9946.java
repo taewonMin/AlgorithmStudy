e31cf8f7e404e84f3ed06711c365e6aba5f0c65c
Ian <gramian7756@gmail.com>
2021-06-16 21:45:38 +0900

    속도 1200

--- AlgorithmStudy/src/week5_sort/psw/Sort_9946.java
+++ AlgorithmStudy/src/week5_sort/psw/Sort_9946.java
@@ -1,13 +1,11 @@
 package week5_sort.psw;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sort_9946 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int i = 1;
 		while (true) {
 			String listStr = sc.next();
@@ -17,24 +15,16 @@ public class Sort_9946 {
 			System.out.println("Case "+i+": "+answer);
 			i++;
 		}
-		
 		sc.close();
 	}
 	
 	public static boolean isEquals(char[] list1, char[] list2) {
-		List<String> list = new ArrayList<String>();
-		List<String> mix = new ArrayList<String>();
 		if(list1.length != list2.length) return false;
 		
-		for (int i = 0; i < list1.length; i++) {
-			list.add(String.valueOf(list1[i]));
-			mix.add(String.valueOf(list2[i]));
-		}
-		list.sort(null);
-		mix.sort(null);
-		for (int i = 0; i < list.size(); i++) {
-			if(!list.get(i).equals(mix.get(i))) return false;
-		}
+		Arrays.sort(list1);
+		Arrays.sort(list2);
+		
+		if (!Arrays.equals(list1, list2)) return false;
 		
 		return true;
 	}
