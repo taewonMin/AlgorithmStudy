fdf48abb88c234374e614bf60a9a8ebad81fce1c
user <user@192.168.219.103>
2021-06-23 20:26:49 +0900

    6주차 커밋

--- AlgorithmStudy/src/week6_sort/chw/Sort_18766.java
+++ AlgorithmStudy/src/week6_sort/chw/Sort_18766.java
@@ -1,5 +1,40 @@
 package week6_sort.chw;
 
-public class Sort_18766 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Sort_18766 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int cnt = in.nextInt();
+		
+		for(int i = 0; i < cnt; i++) {
+			int count = in.nextInt();
+			String[] firArr = new String[count];
+			String[] secArr = new String[count];
+			for(int j = 0; j < count; j++) {
+				firArr[j] = in.next();
+			}
+			for(int j = 0; j < count; j++) {
+				secArr[j] = in.next();
+			}
+			Arrays.sort(firArr);
+			Arrays.sort(secArr);
+			
+			// 배열 --> 문자열
+			String fir = Arrays.toString(firArr);
+			String sec = Arrays.toString(secArr);
+			
+			if(fir.equals(sec)) {
+				System.out.println("NOT CHEATER");
+			}else {
+				System.out.println("CHEATER");
+			}
+			
+		}
+		
+		
+	}
 }
