0c696b94ffcc5171542635a8f50ea4d64f6e5f61
user <user@192.168.219.105>
2021-06-28 02:58:31 +0900

    7주차 중간 커밋

--- AlgorithmStudy/src/week7_sort/chw/Sort_15720.java
+++ AlgorithmStudy/src/week7_sort/chw/Sort_15720.java
@@ -1,5 +1,37 @@
 package week7_sort.chw;
 
-public class Sort_15720 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Sort_15720 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int hamburgers = in.nextInt();
+		int sides = in.nextInt();
+		int drinks = in.nextInt();
+		
+		int[] hamburgerArr = new int[hamburgers];
+		int[] sideArr = new int[sides];
+		int[] drinkArr = new int[drinks];
+		
+		for(int i = 0; i < hamburgers; i++) {
+			hamburgerArr[i] = in.nextInt();
+		}
+		for(int i = 0; i < sides; i++) {
+			sideArr[i] = in.nextInt();
+		}
+		for(int i = 0; i < drinks; i++) {
+			drinkArr[i] = in.nextInt();
+		}
+		
+		Arrays.sort(hamburgerArr);
+		Arrays.sort(sideArr);
+		Arrays.sort(drinkArr);
+		
+		
+		
+		in.close();
+	}
 }
--- AlgorithmStudy/src/week7_sort/chw/Sort_3845.java
+++ AlgorithmStudy/src/week7_sort/chw/Sort_3845.java
@@ -1,5 +1,41 @@
 package week7_sort.chw;
 
-public class Sort_3845 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Sort_3845 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int nx = 0;
+		int ny = 0;
+		float width = 0;
+		
+		while(true) {
+			nx = in.nextInt();
+			ny = in.nextInt();
+			width = in.nextFloat();
+			
+			if(nx == 0 && ny == 0 && width == 0.0) {
+				break;
+			}
+			
+			float[] horizontal = new float[nx];
+			float[] vertical = new float[ny];
+
+			for(int i = 0; i < nx; i++) {
+				horizontal[i] = in.nextFloat();
+			}
+			
+			for(int i = 0; i < ny; i++) {
+				vertical[i] = in.nextFloat();
+			}
+			
+			Arrays.sort(horizontal);
+			Arrays.sort(vertical);
+			// 0번째 += width 총합 비교하면 될듯
+		}
+		
+		in.close();
+	}
 }
