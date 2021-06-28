c2723bba4dc25e01b2bf25fda40d9fcfe0fc6db6
Ia7756 <ian@192.168.0.7>
2021-06-28 23:52:43 +0900

    test

--- AlgorithmStudy/src/week7_sort/psw/Sort_3845.java
+++ AlgorithmStudy/src/week7_sort/psw/Sort_3845.java
@@ -1,5 +1,6 @@
 package week7_sort.psw;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sort_3845 {
@@ -10,6 +11,7 @@ public class Sort_3845 {
 			float[] nx = new float[sc.nextInt()];
 			float[] ny = new float[sc.nextInt()];
 			float width = sc.nextFloat();
+			boolean answer = true;
 			
 			for (int i = 0; i < nx.length; i++) {
 				nx[i] = sc.nextFloat();
@@ -18,9 +20,27 @@ public class Sort_3845 {
 				ny[i] = sc.nextFloat();
 			}
 			
+			Arrays.sort(nx);
+			Arrays.sort(ny);
 			
+			for (int i = 0; i < nx.length; i++) {
+				if(nx[i] > (75/nx.length)*(i+1) + width/2 || nx[i] < (75/nx.length)*(i+1) -width/2) {
+					answer = false;
+					break;
+				}
+			}
+			
+			if(!answer) {
+				for (int i = 0; i < ny.length; i++) {
+					if(ny[i] > (100/ny.length)*(i+1) + width/2 || ny[i] < (100/ny.length)*(i+1) -width/2) {
+						answer = false;
+						break;
+					}
+				}
+			}
 			
 			if(nx.length == 0 && ny.length ==0 && width == 0 ) break;
+			System.out.println(answer ? "YES" : "NO");
 		}
 		
 		sc.close();
