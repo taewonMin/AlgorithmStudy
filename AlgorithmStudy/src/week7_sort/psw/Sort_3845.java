ee3d45f06fc502dfcc371f817d2c91368c17e227
Ian <gramian7756@gmail.com>
2021-06-28 21:49:22 +0900

    as

--- AlgorithmStudy/src/week7_sort/psw/Sort_3845.java
+++ AlgorithmStudy/src/week7_sort/psw/Sort_3845.java
@@ -1,5 +1,28 @@
 package week7_sort.psw;
 
-public class Sort_3845 {
+import java.util.Scanner;
 
+public class Sort_3845 {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		
+		while (true) {
+			float[] nx = new float[sc.nextInt()];
+			float[] ny = new float[sc.nextInt()];
+			float width = sc.nextFloat();
+			
+			for (int i = 0; i < nx.length; i++) {
+				nx[i] = sc.nextFloat();
+			}
+			for (int i = 0; i < ny.length; i++) {
+				ny[i] = sc.nextFloat();
+			}
+			
+			
+			
+			if(nx.length == 0 && ny.length ==0 && width == 0 ) break;
+		}
+		
+		sc.close();
+	}
 }
