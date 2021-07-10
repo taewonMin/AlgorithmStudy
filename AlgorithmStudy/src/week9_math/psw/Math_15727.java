8028f5f1497751326c465d5ab542afcce53c1527
Ian <gramian7756@gmail.com>
2021-07-10 17:18:35 +0900

    9주

--- AlgorithmStudy/src/week9_math/psw/Math_15727.java
+++ AlgorithmStudy/src/week9_math/psw/Math_15727.java
@@ -1,5 +1,12 @@
 package week9_math.psw;
 
-public class Math_15727 {
+import java.util.Scanner;
 
+public class Math_15727 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		System.out.println(l%5 > 0 ? l/5 + 1 : l/5);
+		sc.close();
+	}
 }
--- AlgorithmStudy/src/week9_math/psw/Math_16673.java
+++ AlgorithmStudy/src/week9_math/psw/Math_16673.java
@@ -1,5 +1,15 @@
 package week9_math.psw;
 
-public class Math_16673 {
+import java.util.Scanner;
 
+public class Math_16673 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt(),k = sc.nextInt(), p = sc.nextInt(),total = 0;
+		for (int i = 1; i <= c; i++) {
+			total += i*(k+i*p);
+		}
+		System.out.println(total);
+		sc.close();
+	}
 }
