5d02c4070e441ce5d3972685a3d4f59b37a1686d
stops <stops@192.168.1.224>
2021-07-11 21:02:52 +0900

    210714
    앵그리창영
    조별과제를 하려는데 조장이 사라졌다
    고려대학교에는 공식 와인이 있다

--- AlgorithmStudy/src/week9_math/jje/Math_15727.java
+++ AlgorithmStudy/src/week9_math/jje/Math_15727.java
@@ -1,5 +1,21 @@
 package week9_math.jje;
 
-public class Math_15727 {
+import java.util.Scanner;
 
+public class Math_15727 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double l = sc.nextDouble();
+		
+		double minute = l/5; 
+		
+		int t = (int) Math.ceil(minute);
+		
+		System.out.println(t);
+
+		sc.close();
+	}
 }
--- AlgorithmStudy/src/week9_math/jje/Math_16673.java
+++ AlgorithmStudy/src/week9_math/jje/Math_16673.java
@@ -1,5 +1,25 @@
 package week9_math.jje;
 
+import java.util.Scanner;
+
 public class Math_16673 {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int c = sc.nextInt(); //와인을 모은 년수
+		int k = sc.nextInt(); //고려대 애착 정도
+		int p = sc.nextInt(); //구매중독
+		
+		int result = 0;
+		
+		for (int i = 1; i <= c; i++) {
+			result+=k*i+p*i*i;
+		}
+		
+		System.out.println(result);
+		
+		sc.close();
+	}
 }
