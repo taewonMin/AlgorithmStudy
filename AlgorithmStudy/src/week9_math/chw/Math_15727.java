5493635f3ccb9d4af6f970cdd12774f86809337e
hyeonwoo <tifhs43@naver.com>
2021-07-12 02:08:32 +0900

    9주차 백준 커밋

--- AlgorithmStudy/src/week9_math/chw/Math_15727.java
+++ AlgorithmStudy/src/week9_math/chw/Math_15727.java
@@ -1,5 +1,12 @@
 package week9_math.chw;
 
-public class Math_15727 {
+import java.util.Scanner;
 
+public class Math_15727 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int distance = in.nextInt();
+		System.out.println(distance % 5 == 0 ? distance / 5 : distance / 5 + 1);
+		in.close();
+	}
 }
--- AlgorithmStudy/src/week9_math/chw/Math_16673.java
+++ AlgorithmStudy/src/week9_math/chw/Math_16673.java
@@ -1,5 +1,22 @@
 package week9_math.chw;
 
-public class Math_16673 {
+import java.util.Scanner;
 
+public class Math_16673 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int years = in.nextInt();
+		int attachment = in.nextInt();
+		int addiction = in.nextInt();
+		
+		int total = 0;
+		for(int i = 1; i <= years; i++) {
+			total += i*(attachment + addiction*i);
+		}
+		System.out.println(total);
+		in.close();
+		
+	}
 }
