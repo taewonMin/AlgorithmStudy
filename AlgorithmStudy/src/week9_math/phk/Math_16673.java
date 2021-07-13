010d97852ad7ed757749e8bacb3e2667ecf47484
user <user@sec>
2021-07-13 11:46:24 +0900

    9주차

--- AlgorithmStudy/src/week9_math/phk/Math_16673.java
+++ AlgorithmStudy/src/week9_math/phk/Math_16673.java
@@ -1,5 +1,25 @@
 package week9_math.phk;
 
-public class Math_16673 {
+import java.util.Scanner;
 
+// 고려대학교에는 공식 와인이 있다
+public class Math_16673 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int c = scan.nextInt();
+		int k = scan.nextInt();
+		int p = scan.nextInt();
+		
+		int sum = 0;
+		int n = 0;
+		
+		while (n <= c) {
+			sum += k*n+p*Math.pow(n, 2);
+			n++;
+		}
+		
+		System.out.println(sum);
+		scan.close();
+	}
 }
