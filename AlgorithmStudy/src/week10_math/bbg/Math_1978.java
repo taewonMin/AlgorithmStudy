5381c4a1246fa647e410e3b06f6a2da82b8390f7
gilbeee <7mini96@naver.com>
2021-07-21 00:44:24 +0900

    1978

--- AlgorithmStudy/src/week10_math/bbg/Math_1978.java
+++ AlgorithmStudy/src/week10_math/bbg/Math_1978.java
@@ -1,5 +1,32 @@
 package week10_math.bbg;
 
-public class Math_1978 {
+import java.util.Scanner;
 
+//소수
+public class Math_1978 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] numbers = new int[sc.nextInt()];
+		
+		int pNCount = 0;
+		for(int cnt = 0 ; cnt < numbers.length ; cnt++) {
+			if(isPrimeNumber(sc.nextInt())) pNCount++;
+		}
+		
+		System.out.println(pNCount);
+		
+		sc.close();
+	}
+	
+	public static boolean isPrimeNumber(int number) {
+		if(number == 1) return false;
+		int end = (int) Math.sqrt(number);
+		
+		for(int cnt = 2 ; cnt <= end ; cnt++) {
+			if(number % cnt == 0) return false;
+		}
+
+		return true;
+	}
 }
