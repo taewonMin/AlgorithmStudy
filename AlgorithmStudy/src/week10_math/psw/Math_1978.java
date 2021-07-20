05412b38dc562238769370f63e8e7e8ac6464f0a
Ian <gramian7756@gmail.com>
2021-07-20 22:53:00 +0900

    소수 찾기

--- AlgorithmStudy/src/week10_math/psw/Math_1978.java
+++ AlgorithmStudy/src/week10_math/psw/Math_1978.java
@@ -1,5 +1,21 @@
 package week10_math.psw;
 
-public class Math_1978 {
+import java.util.Scanner;
 
+public class Math_1978 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int answer = n;
+    	for (int i = 0; i < n; i++) {
+    		int num = sc.nextInt();
+    		if(num == 1) answer--;
+    		int x = (int)Math.sqrt(num)+1;
+    		
+			for (int j = 2; j < x; j++) { if(num%j == 0) { answer--; break; } }
+			
+		}
+    	sc.close();
+    	System.out.println(answer);
+    }
 }
