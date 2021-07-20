40cc4aa9cf9f0497c83ab4d8c65b0635d5e43284
gilbeee <7mini96@naver.com>
2021-07-20 23:56:09 +0900

    17294

--- AlgorithmStudy/src/week10_math/bbg/Math_17294.java
+++ AlgorithmStudy/src/week10_math/bbg/Math_17294.java
@@ -1,30 +1,22 @@
 package week10_math.bbg;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Scanner;
 
-//귀여운 //다시커밋
+//귀여운
 public class Math_17294 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		BigDecimal num = new BigDecimal(sc.next());
-		int length = num.precision();
-		BigDecimal[] numArr = new BigDecimal[length]; // 정수만 준다는 가정 하에
-		int idx = length-1;
-		while(idx >= 0) {
-			BigDecimal temp = num.remainder(new BigDecimal("10"));
-			numArr[idx--] = temp;
-			num = num.divide(new BigDecimal("10")).setScale(0, RoundingMode.FLOOR);
-		}
-		BigDecimal dist = BigDecimal.ZERO;
-		if(length != 1)  dist = numArr[0].subtract(numArr[1]);
-		boolean flag = true; 
-		idx = 1;
-		while(length != 1 && flag && idx < length-1) flag = numArr[idx].subtract(dist) == numArr[idx++ +1] ? true : false;
-
-		System.out.println(length == 1 || (idx == length-1 && flag ) ? "◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!" : "흥칫뿡!! <(￣ ﹌ ￣)>");
+		String num = sc.next();
+		
+		String[] arr = num.split("");
+		float dist = 0;
+		if(arr.length != 1) dist = (float) (Integer.parseInt(arr[0]) - Integer.parseInt(arr[1]));
+		boolean flag = true;
+		int count = 0;
+		while(arr.length != 1 && flag && count < arr.length-1) flag = Integer.parseInt(arr[count]) - dist == Integer.parseInt(arr[count++ +1]) ? true : false;
+			
+		System.out.println(arr.length == 1 || (count == arr.length-1 && flag ) ? "◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!" : "흥칫뿡!! <(￣ ﹌ ￣)>");
 		
 		sc.close();
 	}
