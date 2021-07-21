8fa86a6bf58fc4c20b269191cc1b866c0bf6dd9a
Ian <gramian7756@gmail.com>
2021-07-21 23:18:10 +0900

    저작권

--- AlgorithmStudy/src/week11_math/psw/Math_2914.java
+++ AlgorithmStudy/src/week11_math/psw/Math_2914.java
@@ -1,5 +1,11 @@
 package week11_math.psw;
 
-public class Math_2914 {
+import java.util.Scanner;
 
+public class Math_2914 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()*(sc.nextInt()-1)+1);
+		sc.close();
+	}
 }
