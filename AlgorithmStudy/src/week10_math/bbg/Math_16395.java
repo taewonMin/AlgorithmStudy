125dd3b1ee5dec14886cc4e5101d60c97b5f0cb0
gilbeee <7mini96@naver.com>
2021-07-21 20:24:38 +0900

    고대로c_16395

--- AlgorithmStudy/src/week10_math/bbg/Math_16395.java
+++ AlgorithmStudy/src/week10_math/bbg/Math_16395.java
@@ -2,7 +2,7 @@ package week10_math.bbg;
 
 import java.util.Scanner;
 
-//파스칼
+//파스칼 17672KB 212ms
 public class Math_16395 {
 	static int[][] pascalArr = new int[30][30]; 
 	public static void main(String[] args) {
@@ -28,3 +28,30 @@ public class Math_16395 {
 		return pascalArr[row-1][col-1];
 	}
 }
+/* 1112KB 0ms
+ #include <stdio.h>
+
+ int main(){
+    int pascalArr[30][30]{}; // 이차원배열 선언 및 초기화({})
+    int row, col;
+    scanf("%d\n", &row);
+    scanf("%d\n", &col);
+    
+    pascalArr[0][0] = 1;
+    for(int r=1 ; r<row ; r++){
+        for(int c=0 ; c<r+1 ; c++){
+            if(c==0){
+                pascalArr[r][c] = pascalArr[r-1][c];
+            }else if(c!=0 && c!= r){
+                pascalArr[r][c] = pascalArr[r-1][c-1] + pascalArr[r-1][c];
+            }else{
+                pascalArr[r][c] = pascalArr[r-1][c-1];
+            }
+        }
+    }
+    
+    printf("%d", pascalArr[row-1][col-1]);
+    
+    return 0;
+ } 
+ */
