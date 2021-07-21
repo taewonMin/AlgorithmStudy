04374a07bdb3a30256814c8c2ed6c9e6a4c5ce65
Ian <gramian7756@gmail.com>
2021-07-21 23:26:24 +0900

    상근버거

--- AlgorithmStudy/src/week11_math/psw/Math_5543.java
+++ AlgorithmStudy/src/week11_math/psw/Math_5543.java
@@ -1,5 +1,11 @@
 package week11_math.psw;
 
-public class Math_5543 {
+import java.util.Scanner;
 
+public class Math_5543 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println( Math.min(sc.nextInt(), Math.min(sc.nextInt(), sc.nextInt())) + Math.min(sc.nextInt(), sc.nextInt()) - 50);
+		sc.close();
+	}
 }
