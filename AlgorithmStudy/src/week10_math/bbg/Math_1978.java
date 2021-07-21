ae5c3d4732a66519865dbcd8a4c1751485353f49
gilbeee <7mini96@naver.com>
2021-07-21 21:00:36 +0900

    그대로c 1978

--- AlgorithmStudy/src/week10_math/bbg/Math_1978.java
+++ AlgorithmStudy/src/week10_math/bbg/Math_1978.java
@@ -2,7 +2,7 @@ package week10_math.bbg;
 
 import java.util.Scanner;
 
-//소수
+//소수 17784KB 240ms
 public class Math_1978 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -30,3 +30,37 @@ public class Math_1978 {
 		return true;
 	}
 }
+/* 1128KB 0ms
+#include <stdio.h>
+#include <math.h>
+
+bool isPrimeNumber(int number){
+    if(number == 1) return false;
+    
+    int end = sqrt(number);
+    for(int cnt = 2 ; cnt <= end ; cnt++){
+        if(number % cnt == 0) return false;
+    }
+    return true;
+}
+
+//소수
+int main(){
+    int n, pNCount, arrlength, temp;
+    scanf("%d\n", &n);
+    
+	int numbers[n]{};
+    pNCount = 0;
+    arrlength = sizeof(numbers)/sizeof(int);
+    
+	for(int cnt=0 ; cnt < arrlength ; cnt++){
+        scanf("%d\n", &temp);
+        if(isPrimeNumber(temp)) pNCount++;
+    }	
+	
+    printf("%d\n", pNCount);
+		
+    return 0;
+}
+
+*/
