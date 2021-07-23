5c3e2415a79fe11baf3f556167b75bfa214ebaa5
Ian <gramian7756@gmail.com>
2021-07-23 09:56:32 +0900

    피보나치 숏코딩 버전

--- AlgorithmStudy/src/week11_math/psw/Math_13270.java
+++ AlgorithmStudy/src/week11_math/psw/Math_13270.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class Math_13270 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
+		int num = sc.nextInt();
+		System.out.println((num%2 == 0 ? num/2 :  ((num-3)/2) + 2) + " " + (num%3 == 0 ? 2*(num/3) :(num%3 == 2 ? 2*(num/3) + 1 :  2*(num/3))));
 		sc.close();
 	}
 }
