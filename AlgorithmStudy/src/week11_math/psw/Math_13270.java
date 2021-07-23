850faeb0c170def53840e4bcc78e3a9fb40e0e7a
Ian <gramian7756@gmail.com>
2021-07-23 09:57:08 +0900

    피보나치 치킨

--- AlgorithmStudy/src/week11_math/psw/Math_13270.java
+++ AlgorithmStudy/src/week11_math/psw/Math_13270.java
@@ -6,7 +6,9 @@ public class Math_13270 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		System.out.println((num%2 == 0 ? num/2 :  ((num-3)/2) + 2) + " " + (num%3 == 0 ? 2*(num/3) :(num%3 == 2 ? 2*(num/3) + 1 :  2*(num/3))));
+		int max = num%3 == 0 ? 2*(num/3) :(num%3 == 2 ? 2*(num/3) + 1 :  2*(num/3)) ;
+		int min = num%2 == 0 ? num/2 :  ((num-3)/2) + 2;
+		System.out.println(min + " " + max);
 		sc.close();
 	}
 }
