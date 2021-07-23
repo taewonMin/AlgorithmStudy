f55d1d7910fc1623478e027acbb31d87c7c81a47
Ian <gramian7756@gmail.com>
2021-07-23 10:17:56 +0900

    피보나치 리팩토링

--- AlgorithmStudy/src/week11_math/psw/Math_13270.java
+++ AlgorithmStudy/src/week11_math/psw/Math_13270.java
@@ -1,14 +1,14 @@
 package week11_math.psw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Math_13270 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int max = num%3 == 0 ? 2*(num/3) :(num%3 == 2 ? 2*(num/3) + 1 :  2*(num/3)) ;
-		int min = num%2 == 0 ? num/2 :  ((num-3)/2) + 2;
-		System.out.println(min + " " + max);
-		sc.close();
+	public static void main(String[] args) throws Exception {
+		int num = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int max = (num%3 == 2 ? 2*(num/3) + 1 : 2*(num/3));
+		int min = (num%2 == 0 ? num/2 :  ((num-3)/2) + 2) ;
+		
+		System.out.println( min + " " + max );
 	}
 }
