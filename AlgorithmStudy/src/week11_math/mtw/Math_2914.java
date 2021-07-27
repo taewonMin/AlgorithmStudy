93cb871d89ff30a5677f740567cc34c2489e631e
taewonMin <mtw9411@naver.com>
2021-07-28 03:20:45 +0900

    2914 저작권

--- AlgorithmStudy/src/week11_math/mtw/Math_2914.java
+++ AlgorithmStudy/src/week11_math/mtw/Math_2914.java
@@ -1,5 +1,17 @@
 package week11_math.mtw;
 
-public class Math_2914 {
+import java.util.Scanner;
 
+// 저작권
+public class Math_2914 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		int avg = sc.nextInt();
+		
+		System.out.println(cnt*(avg-1)+1);
+		
+		sc.close();
+	}
 }
