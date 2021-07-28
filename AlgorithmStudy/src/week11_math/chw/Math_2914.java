b6cc3ccf83f4b47641b07a2ddd4afd1aff6f874b
hyeonwoo <tifhs43@naver.com>
2021-07-28 19:14:47 +0900

    11주차 알고리즘 커밋

--- AlgorithmStudy/src/week11_math/chw/Math_2914.java
+++ AlgorithmStudy/src/week11_math/chw/Math_2914.java
@@ -1,5 +1,17 @@
 package week11_math.chw;
 
-public class Math_2914 {
+import java.util.Scanner;
 
+public class Math_2914 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int musicCnt = in.nextInt();
+		int avg = in.nextInt();
+		
+		System.out.println(musicCnt * (avg - 1) + 1);
+		
+		in.close();
+	}
 }
--- AlgorithmStudy/src/week11_math/chw/Math_5543.java
+++ AlgorithmStudy/src/week11_math/chw/Math_5543.java
@@ -1,5 +1,23 @@
 package week11_math.chw;
 
-public class Math_5543 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Math_5543 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] burgers = new int[3];
+		int[] drinks = new int[2];
+		for(int i = 0; i < 5; i++) {
+			if(i < 3) {
+				burgers[i] = in.nextInt(); 
+			}else {
+				drinks[i-3] = in.nextInt();
+			}
+		}
+		Arrays.sort(burgers);
+		Arrays.sort(drinks);
+		System.out.println(burgers[0] + drinks[0] - 50);
+		in.close();
+	}
 }
