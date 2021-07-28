e0a7ca706c9f1058ec56628485635ae0943e25d1
gilbeee <7mini96@naver.com>
2021-07-28 20:16:31 +0900

    5543

--- AlgorithmStudy/src/week11_math/bbg/Math_5543.java
+++ AlgorithmStudy/src/week11_math/bbg/Math_5543.java
@@ -1,5 +1,28 @@
 package week11_math.bbg;
 
-public class Math_5543 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+//상근날드 17588KB 212ms
+public class Math_5543 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] burgers = new int[3];
+		int[] beverages = new int[2];
+		
+		for(int idx = 0 ; idx < burgers.length ; idx++) {
+			burgers[idx] = sc.nextInt();
+		}
+		for(int idx = 0 ; idx < beverages.length ; idx++) {
+			beverages[idx] = sc.nextInt();
+		}
+		
+		Arrays.sort(burgers);
+		Arrays.sort(beverages);
+		
+		System.out.println(burgers[0] + beverages[0] - 50);
+		
+		sc.close();
+	}
 }
