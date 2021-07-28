94ec4853c8524aa2ec5972bf43aa7e7759abe1e5
Ian <gramian7756@gmail.com>
2021-07-28 23:13:45 +0900

    할오윈

--- AlgorithmStudy/src/week12_math/psw/Math_10178.java
+++ AlgorithmStudy/src/week12_math/psw/Math_10178.java
@@ -1,10 +1,16 @@
 package week12_math.psw;
 
 import java.util.Scanner;
+import java.util.function.BiConsumer;
 
 public class Math_10178 {
+	
+	public final static BiConsumer<Integer, Integer> printAnswer = (x, y) -> System.out.println("You get "+x/y+" piece(s) and your dad gets "+x%y+" piece(s).");
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) { printAnswer.accept(sc.nextInt(), sc.nextInt()); }
 		sc.close();
 	}
 }
