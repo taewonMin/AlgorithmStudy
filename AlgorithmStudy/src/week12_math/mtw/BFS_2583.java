a1ba5a782b4960ffcf53c54855faa440f7b47a39
Ian <gramian7756@gmail.com>
2021-07-29 23:26:13 +0900

    심화

---
+++ AlgorithmStudy/src/week12_math/mtw/BFS_2583.java
@@ -0,0 +1,12 @@
+package week12_math.mtw;
+
+import java.util.Scanner;
+
+public class BFS_2583 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+}
