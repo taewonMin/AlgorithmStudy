655daed0da82d769937128391638952c5e582738
gilbeee <7mini96@naver.com>
2021-08-01 23:49:24 +0900

    패키징 편해졌네유 굿 2163

--- AlgorithmStudy/src/week12_math/bbg/Math_2163.java
+++ AlgorithmStudy/src/week12_math/bbg/Math_2163.java
@@ -1,10 +1,24 @@
 package week12_math.bbg;
 
 import java.util.Scanner;
-
+//초콜릿 17660KB	244ms
 public class Math_2163 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println((sc.nextInt() * sc.nextInt()) -1);
+		
 		sc.close();
 	}
 }
+/* 2020KB	0ms
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b ;
+    cout << a * b - 1 << endl;
+    return 0;
+}
+*/
