c0364d3aadc04a8461c5c904f8879bc8e6ca86cb
user <user@192.168.219.101>
2021-08-02 23:21:23 +0900

    12주차 백준 커밋

--- AlgorithmStudy/src/week12_math/chw/Math_2163.java
+++ AlgorithmStudy/src/week12_math/chw/Math_2163.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Math_2163 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()*sc.nextInt()-1);
 		sc.close();
 	}
 }
--- AlgorithmStudy/src/week12_math/chw/Math_3036.java
+++ AlgorithmStudy/src/week12_math/chw/Math_3036.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Math_3036 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		int firstRing = sc.nextInt();
+		for(int i = 0; i < cnt-1; i++) {
+			int ring = sc.nextInt();
+			for(int j = ring; j > 0; j--) {
+				if (firstRing % j == 0 && ring % j == 0) {
+					System.out.println(firstRing / j + "/" + ring / j);
+					break;
+				}
+			}
+		}
 		sc.close();
 	}
 }
