c2e1a7fe756d0f9d3ae378ec76586f78a5baaa61
stops <stops@DESKTOP-AFPDL7V>
2021-08-03 22:04:04 +0900

    할로윈의 사탕

--- AlgorithmStudy/src/week12_math/jje/Math_10178.java
+++ AlgorithmStudy/src/week12_math/jje/Math_10178.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Math_10178 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		sc.close();
-	}
+		
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+      	  int c = sc.nextInt();
+      	  int v = sc.nextInt();
+      	  System.out.println("You get "+c/v+" piece(s) and your dad gets "+c%v+" piece(s).");
+        }
+    }
 }
