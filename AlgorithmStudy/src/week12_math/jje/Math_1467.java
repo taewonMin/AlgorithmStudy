8a38a44781a144ef65bfec9a4317c83cb558d2cd
stops <stops@DESKTOP-AFPDL7V>
2021-08-04 19:59:46 +0900

    날짜 계산

--- AlgorithmStudy/src/week12_math/jje/Math_1467.java
+++ AlgorithmStudy/src/week12_math/jje/Math_1467.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class Math_1467 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		sc.close();
+		
+        int e = sc.nextInt();
+        int s = sc.nextInt();
+        int m = sc.nextInt();
+        int i = 1;
+        
+        while(true){ 
+        	
+              if((i-e)%15==0 && (i-s)%28==0 && (i-m)%19==0){
+                    System.out.println(i); 
+                    break;
+                 } 
+              i++;
+        }
+        
+        sc.close();
 	}
 }
