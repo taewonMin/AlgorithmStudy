36c02996f460d9e547bdf84144e73f26cc3057ef
hyeonwoo <tifhs43@naver.com>
2021-08-06 11:49:38 +0900

    파이썬 쉽게 푸는 문제 (예제만 맞음)

--- AlgorithmStudy/src/week13/chw/Math_1292.java
+++ AlgorithmStudy/src/week13/chw/Math_1292.java
@@ -10,3 +10,28 @@ public class Math_1292 {
 		sc.close();
 	}
 }
+/*
+파이썬
+def make_arr(B, arr):
+    total = 0
+    idx = 1
+    while True:
+        total += idx
+        if int(B) < total:
+            break
+        idx+=1
+    for i in range(idx):
+        for _ in range(i+1):
+            arr.append(i+1)   
+    return arr
+
+A, B = input().split()
+
+result = 0
+arr = make_arr(B, [])
+for i in range(int(A)-1,int(B)):
+    result += arr[int(A)]
+print(result)
+
+
+*/
