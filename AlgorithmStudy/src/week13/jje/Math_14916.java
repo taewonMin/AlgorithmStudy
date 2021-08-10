093452bb77b45811a76259dbdb4d6b12dd37cb0a
Jieun Jeong <stopsilver212@gmail.com>
2021-08-10 15:02:58 +0900

    거스름돈

--- AlgorithmStudy/src/week13/jje/Math_14916.java
+++ AlgorithmStudy/src/week13/jje/Math_14916.java
@@ -4,9 +4,31 @@ import java.util.Scanner;
 
 public class Math_14916 {
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		
-		
+		int money = sc.nextInt();
+
+		if(money==1||money==3)System.out.println("-1");
+		else countChange(money);
+
 		sc.close();
+		//만약 거슬러 줄 수 없으면 -1을 출력한다.
+
+		//13 -> 5
+		//14 -> 4
+		//5의 배수가 될 때까지 2를 뺀다 빼면서 개수 세기
 	}
+
+	static int num = 0;
+
+	static void countChange(int money) {
+		if(money%5==0) {
+			System.out.println(money/5+num); return;
+		}
+		money-=2;
+		num++;
+		countChange(money);
+	}
+
 }
+
