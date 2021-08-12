b5f861f5928a7973841009add4c695d69e157e13
Ian <gramian7756@gmail.com>
2021-08-12 16:40:44 +0900

    문제 변경

--- AlgorithmStudy/src/part2/week14/jje/DS_1021.java
+++ AlgorithmStudy/src/part2/week14/jje/DS_1021.java
@@ -1,8 +1,8 @@
-package part2.week14.chw;
+package part2.week14.jje;
 
 import java.util.Scanner;
 
-public class Math_6603 {
+public class DS_1021 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
--- AlgorithmStudy/src/part2/week14/mtw/DS_1021.java
+++ AlgorithmStudy/src/part2/week14/mtw/DS_1021.java
@@ -2,7 +2,7 @@ package part2.week14.mtw;
 
 import java.util.Scanner;
 
-public class Math_6603 {
+public class DS_1021 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
