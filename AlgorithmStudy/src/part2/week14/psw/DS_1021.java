8971b9364bdf4ebf7ea516df7cb3150919b25395
Ian <gramian7756@gmail.com>
2021-08-15 16:32:02 +0900

    회전큐, 비밀번호

--- AlgorithmStudy/src/part2/week14/psw/DS_1021.java
+++ AlgorithmStudy/src/part2/week14/psw/DS_1021.java
@@ -1,10 +1,34 @@
 package part2.week14.psw;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class DS_1021 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int answer = 0;
+		
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		
+		for (int i = 0; i < n; i++) queue.add(i+1); 
+		
+		
+		for (int i = 0; i < m; i++) {
+			Integer targetIdx = sc.nextInt();
+			
+
+			while (targetIdx != queue.peek()) {			
+				if(queue.indexOf(targetIdx) <= queue.size()/2)  queue.addLast(queue.removeFirst()); // 2번 연산
+ 				else  queue.addFirst(queue.removeLast()); // 3번 연산
+				answer++;
+			}
+			queue.poll();
+			
+		}
+		System.out.println(answer);
 		
 		sc.close();
 	}
--- AlgorithmStudy/src/part2/week14/psw/DS_17219.java
+++ AlgorithmStudy/src/part2/week14/psw/DS_17219.java
@@ -1,10 +1,22 @@
 package part2.week14.psw;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DS_17219 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		Map<String, String> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			map.put(sc.next(), sc.next());
+		}
+		
+		for (int i = 0; i < m; i++) {
+			System.out.println(map.get(sc.next()));
+		}
 		
 		sc.close();
 	}
