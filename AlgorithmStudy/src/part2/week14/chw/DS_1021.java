de91bce7eeb2d80284ff081b656484a106ea865c
hyeonwoo <hyeonwoo@hyeonwoo-ThinkStation-P320>
2021-08-18 09:48:39 +0900

    14주차 회전하는 큐, 비밀번호 찾기 커밋

--- AlgorithmStudy/src/part2/week14/chw/DS_1021.java
+++ AlgorithmStudy/src/part2/week14/chw/DS_1021.java
@@ -1,10 +1,50 @@
 package part2.week14.chw;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class DS_1021 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int cnt = sc.nextInt();
+		int result = 0;
+		Deque<Integer> deque = new ArrayDeque<>();
+		for(int i = 1; i <= size; i++) {
+			deque.add(i);
+		}
+		
+		for(int i = 0; i < cnt; i++) {
+			int number = sc.nextInt();
+			boolean reverse = true;
+			Iterator<Integer> iter = deque.iterator();
+			int checkOrder = 0;
+			while(checkOrder <= deque.size() / 2) {
+				int num = iter.next();
+				if(number == num) {
+					reverse = false;
+					break;
+				}
+				checkOrder++;
+			}
+			
+			while(true) {
+				if(deque.peek() == number) {
+					deque.removeFirst();
+					break;
+				}
+				
+				if(reverse) {
+					deque.addFirst(deque.pollLast());
+				}else {
+					deque.addLast(deque.pollFirst());
+				}
+				result++;
+			}
+		}
+		System.out.println(result);
 		
 		sc.close();
 	}
--- AlgorithmStudy/src/part2/week14/chw/DS_17219.java
+++ AlgorithmStudy/src/part2/week14/chw/DS_17219.java
@@ -1,11 +1,30 @@
 package part2.week14.chw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DS_17219 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String[] cnt = bf.readLine().split(" ");
+		int address = Integer.parseInt(cnt[0]);
+		int site = Integer.parseInt(cnt[1]);
+		Map<String, String> map = new HashMap<String, String>();
+		for(int i = 0; i < address; i++) {
+			String[] addr = bf.readLine().split(" ");
+			map.put(addr[0], addr[1]);
+		}
 		
-		sc.close();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i = 0; i < site; i++) {
+			bw.write(map.get(bf.readLine()) +"\n");
+		}
+		bw.flush();
+		bw.close();
 	}
 }
