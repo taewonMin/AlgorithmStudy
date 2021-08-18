d798a26cf6d57ee68a96a2c012005d53406013f5
Ia7756 <gramian7756@gmail.com>
2021-08-18 21:53:41 +0900

    패키징

---
+++ AlgorithmStudy/src/part2/week15/chw/Math_16162.java
@@ -0,0 +1,11 @@
+package part2.week15.chw;
+
+import java.util.Scanner;
+
+public class Math_16162 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+	}
+}
---
+++ AlgorithmStudy/src/part2/week15/jje/DS_10773.java
@@ -0,0 +1,11 @@
+package part2.week15.jje;
+
+import java.util.Scanner;
+
+public class DS_10773 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+	}
+}
