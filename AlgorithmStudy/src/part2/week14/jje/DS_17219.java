3dbf5e4a8283db316ee0c092ccb7c57ccaba643f
stops <stopsilver212@gmail.com>
2021-08-18 20:31:34 +0900

    비밀번호 찾기

--- AlgorithmStudy/src/part2/week14/jje/DS_17219.java
+++ AlgorithmStudy/src/part2/week14/jje/DS_17219.java
@@ -1,11 +1,23 @@
 package part2.week14.jje;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class DS_17219 {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		
+
+		HashMap<String, String> passwordMap = new HashMap<String, String>();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		for(int i = 0; i < n; i++) {
+			passwordMap.put(sc.next(), sc.next());
+		}
+
+		for (int i = 0; i < m; i++) {
+			System.out.println(passwordMap.get(sc.next()));
+		}
 		sc.close();
 	}
 }
