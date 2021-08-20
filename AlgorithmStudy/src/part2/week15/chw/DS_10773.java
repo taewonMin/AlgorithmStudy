c7c2c502605a962fbc93a96ad255c2cb04e6dd26
hyeonwoo <tifhs43@naver.com>
2021-08-20 14:06:40 +0900

    15주차 백준 커밋

--- AlgorithmStudy/src/part2/week15/chw/DS_10773.java
+++ AlgorithmStudy/src/part2/week15/chw/DS_10773.java
@@ -1,11 +1,28 @@
 package part2.week15.chw;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class DS_10773 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+	
+		int cnt = sc.nextInt();
+		int result = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int i = 0; i < cnt; i++) {
+			int number = sc.nextInt();
+			if(number == 0) {
+				stack.pop();
+			}else {
+				stack.add(number);
+			}
+		}
+		int l = stack.size();
+		for(int i = 0; i < l; i++) {
+			result += stack.pop();
+		}
+		System.out.println(result);
 		sc.close();
 	}
 }
--- AlgorithmStudy/src/part2/week15/chw/Math_16162.java
+++ AlgorithmStudy/src/part2/week15/chw/Math_16162.java
@@ -6,6 +6,17 @@ public class Math_16162 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int cnt = sc.nextInt();
+		int fir = sc.nextInt();
+		int gongcha = sc.nextInt();
+		int result = 0;
+		for(int i = 0; i < cnt; i++) {
+			int input = sc.nextInt();
+			if(input != fir) continue;
+			fir += gongcha;
+			result++;
+		}
+		System.out.println(result);
 		sc.close();
 	}
 }
