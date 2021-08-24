3adfe3b0958618939afc3d299b7145986b818a1c
Jieun Jeong <stopsilver212@gmail.com>
2021-08-24 12:42:11 +0900

    제로

--- AlgorithmStudy/src/part2/week15/jje/Math_16162.java
+++ AlgorithmStudy/src/part2/week15/jje/Math_16162.java
@@ -1,11 +1,30 @@
 package part2.week15.jje;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Math_16162 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
+		int k = sc.nextInt();
+
+		Stack<Integer> studyStack = new Stack<Integer>();
+
+		for (int i = 0; i < k; i++) {
+			int num = sc.nextInt();
+			if(num==0) studyStack.pop();
+			else studyStack.push(num);
+		}
+
+		int result = 0;
+		int temp = studyStack.size();
+
+		for (int i = 0; i < studyStack.size(); i++) {
+			result+=studyStack.get(i);
+		}
+
+		System.out.println(result);
 		sc.close();
 	}
 }
