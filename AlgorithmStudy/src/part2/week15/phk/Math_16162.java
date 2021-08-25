cb4971c42197bbfcbde4afb5ac3f841db5962f1e
spring531 <spring_is_coming_531@naver.com>
2021-08-25 21:03:07 +0900

    고음

--- AlgorithmStudy/src/part2/week15/phk/Math_16162.java
+++ AlgorithmStudy/src/part2/week15/phk/Math_16162.java
@@ -2,9 +2,30 @@ package part2.week15.phk;
 
 import java.util.Scanner;
 
+// 39732kb	600ms
 public class Math_16162 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int a = sc.nextInt();
+		int d = sc.nextInt();
+		
+		int x = 0;
+		int a_n = a;
+		
+		// int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			// arr[i] = sc.nextInt();
+			if(sc.nextInt() == a_n) {
+				x++;
+				a_n += d;
+			}
+			
+		}
+		
+		System.out.println(x);
+		
 		
 		sc.close();
 	}
