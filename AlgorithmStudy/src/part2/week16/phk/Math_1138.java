4f10eb115a6b5e582f02cad38f9023a1cdd89451
Ia7756 <gramian7756@gmail.com>
2021-08-25 22:02:56 +0900

    16주 !!

---
+++ AlgorithmStudy/src/part2/week16/phk/Math_1138.java
@@ -0,0 +1,11 @@
+package part2.week16.phk;
+
+import java.util.Scanner;
+
+public class Math_1138 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+	}
+}
---
+++ AlgorithmStudy/src/part2/week16/psw/DS_1966.java
@@ -0,0 +1,11 @@
+package part2.week16.psw;
+
+import java.util.Scanner;
+
+public class DS_1966 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+	}
+}
