8387c304d6828f31df83f91fc81d4a9043accf8a
Ia7756 <gramian7756@gmail.com>
2021-08-26 09:47:49 +0900

    최소 힙

--- AlgorithmStudy/src/part2/week16/psw/DS_1927.java
+++ AlgorithmStudy/src/part2/week16/psw/DS_1927.java
@@ -1,11 +1,25 @@
 package part2.week16.psw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class DS_1927 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> list  = new PriorityQueue<>();
 		
-		sc.close();
+		for (int i = 0; i < n; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				System.out.println(list.isEmpty() ? 0 : list.poll() );
+			}else {
+				list.offer(x);
+			}
+		}
+		
+		br.close();
 	}
 }
