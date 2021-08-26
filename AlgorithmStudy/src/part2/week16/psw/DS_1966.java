d8a46cc7759f94236c710015efaf5acae6e54816
Ia7756 <gramian7756@gmail.com>
2021-08-26 23:42:46 +0900

    프린터

--- AlgorithmStudy/src/part2/week16/psw/DS_1966.java
+++ AlgorithmStudy/src/part2/week16/psw/DS_1966.java
@@ -1,10 +1,43 @@
 package part2.week16.psw;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class DS_1966 {
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		int caseCnt = sc.nextInt();
+		for (int i = 0; i < caseCnt; i++) {
+			
+			int n = sc.nextInt();
+			int idx = sc.nextInt();
+			
+			Queue<Integer[]> priorities = new LinkedList<Integer[]>();
+			for (int j = 0; j < n; j++) {
+				priorities.offer(new Integer[]{j,sc.nextInt()});
+			}
+			
+			int answer = 0;
+			while(!priorities.isEmpty()) {
+				Integer[] poll = priorities.poll();
+				boolean isPrinted = true;
+				for (Integer[] integer : priorities) {
+					if(integer[1] > poll[1]) {
+						isPrinted = false;
+						priorities.offer(poll);
+						break;
+					}
+				}
+				
+				if(isPrinted) answer++;
+				if(isPrinted && poll[0] == idx) break;
+			}
+			
+			System.out.println(answer);
+		}
 		
 		sc.close();
 	}
