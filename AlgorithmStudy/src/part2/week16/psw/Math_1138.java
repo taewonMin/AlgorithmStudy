0154ddff7fd2ee37ec6d9f045519577a6ffb6227
Ia7756 <gramian7756@gmail.com>
2021-08-28 14:35:47 +0900

    섬의 개수

--- AlgorithmStudy/src/part2/week16/psw/Math_1138.java
+++ AlgorithmStudy/src/part2/week16/psw/Math_1138.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Math_1138 {
+	static Stack<Integer[]> list = new Stack<>();
 	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		Stack<Integer[]> list = new Stack<>();
-		LinkedList<Integer[]> answer = new LinkedList<>();
-		
 		for (int i = 0; i < n; i++) {
 			list.push(new Integer[] {sc.nextInt(), i+1});
  		}
 		
+		LinkedList<Integer[]> answer = new LinkedList<>();
 		while(!list.isEmpty()) {
 			Integer[] item = list.pop();
 			for (int i = 0; i <= answer.size(); i++) {
 				if(item[0] == i) answer.add(i, item);
 			}
 		}
-		
 		answer.forEach( (item) -> System.out.print(item[1] +" "));
+		
 		sc.close();
 	}
 }
---
+++ AlgorithmStudy/src/part2/week16/psw/Search_4963.java
@@ -0,0 +1,56 @@
+package part2.week16.psw;
+
+import java.util.Scanner;
+
+public class Search_4963 {
+	static int[][] map;
+	static boolean[][] visitied;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(init(sc)) {
+			int answer = 0;
+			
+			for (int i = 0; i < map.length; i++) {
+				for (int j = 0; j < map[i].length; j++) {
+					if(map[i][j] == 1 && !visitied[i][j]) {
+						dfs(i,j);
+						answer++;
+					}
+				}
+			}
+
+			System.out.println(answer);
+		}
+
+		sc.close();
+	}
+	private static boolean init(Scanner sc) {
+		int w = sc.nextInt(), h = sc.nextInt();
+		if(w+h == 0) return false;
+		
+		visitied = new boolean[h][w];
+		map = new int[h][w];
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		return true;
+	}
+	
+	static void dfs(int i, int j) {
+		visitied[i][j] = true;
+		for (int h = -1; h <= 1; h++) { // 총 8번의 케이스 (8방위 검사)
+			for (int w = -1; w <= 1; w++) {
+				if(h == 0 && w == 0) continue;
+				if(boundaryChk(i+h,j+w) && map[i+h][j+w] == 1 && !visitied[i+h][j+w]) dfs(i+h,j+w);
+			}
+		}
+	}
+	
+	private static boolean boundaryChk(int i, int j) {
+		if(i > -1 && j > -1 && i < map.length && j < map[0].length) return true;
+		return false;
+	}
+}
