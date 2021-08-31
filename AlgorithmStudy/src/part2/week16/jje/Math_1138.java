eb0a782e2f6fdc5535da95d5f50c08ac244b495c
stops <stopsilver212@gmail.com>
2021-08-31 21:29:33 +0900

    한 줄로 서기

--- AlgorithmStudy/src/part2/week16/jje/Math_1138.java
+++ AlgorithmStudy/src/part2/week16/jje/Math_1138.java
@@ -1,11 +1,48 @@
 package part2.week16.jje;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Math_1138 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int humanNum = sc.nextInt();
+		ArrayList<Integer> line = new ArrayList<>();
+		int[] reverseLine = new int[humanNum];
+		for (int i = 0; i < humanNum ; i++) {
+			reverseLine[i] = sc.nextInt();
+		}
+		 for (int i = humanNum; i >= 1 ; i--) {
+		 	line.add(reverseLine[i-1], i);
+		 }
+
+		for (int result:line) {
+			System.out.println(result);
+		}
+		/**
+		 for (int i = 1; i <= humanNum ; i++) {
+		 int count = 0;
+		 int num = sc.nextInt();
+		 for (int j = 0; j < humanNum; j++) {
+		 if (line[j] > i) {
+		 count++;
+		 }
+		 int temp = j+1;
+		 if(temp == humanNum){
+		 temp = 0;
+		 }
+		 if (count == num && line[temp] == humanNum) {
+		 line[temp] = i;
+		 break;
+		 }else if(count == num){
+		 count--;
+		 }
+		 }
+		 }
+		 for (int result:line) {
+		 System.out.println(result);
+		 }
+		 **/
 		sc.close();
 	}
 }
