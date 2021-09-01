b3a1d7cda37e4173150a2ba6320a61da73f3733e
smartoc <smartoc@220.123.146.229>
2021-09-01 21:24:10 +0900

    한 줄로 서기

--- AlgorithmStudy/src/part2/week16/mtw/Math_1138.java
+++ AlgorithmStudy/src/part2/week16/mtw/Math_1138.java
@@ -6,6 +6,27 @@ public class Math_1138 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int N = sc.nextInt();
+		String[] arr = new String[N];
+		int height = 1;
+		for(int i=0; i<N; i++) {
+			int left = sc.nextInt();
+			for(int j=0; j<left; j++) {
+				if(arr[j] != null) {
+					left++;
+				}
+			}
+			while(true) {
+				if(arr[left] == null) {
+					arr[left] = String.valueOf(height++);
+					break;
+				}
+				left++;
+			}
+		}
+		
+		System.out.println(String.join(" ", arr));
+		
 		sc.close();
 	}
 }
