dd957be176dda157a016f3095c845d848190c47c
hyeonwoo <tifhs43@naver.com>
2021-09-01 20:58:34 +0900

    흐음..

--- AlgorithmStudy/src/part2/week16/chw/DS_1927.java
+++ AlgorithmStudy/src/part2/week16/chw/DS_1927.java
@@ -1,11 +1,32 @@
 package part2.week16.chw;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class DS_1927 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int cnt = sc.nextInt();
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for(int i = 0; i < cnt; i++) {
+			int input = sc.nextInt();
+			if (input == 0 && list.size() == 0) {
+				System.out.println(0);
+			}else if(input == 0) {
+				System.out.println(list.poll());
+			}else {
+				Iterator<Integer> iter = list.iterator();
+				int idx = 0;
+				while(iter.hasNext()) {
+					if(iter.next() > input) {
+						break;
+					}
+					idx++;
+				}
+				list.add(idx,input);
+			}
+		}
 		sc.close();
 	}
 }
--- AlgorithmStudy/src/part2/week16/chw/Math_1138.java
+++ AlgorithmStudy/src/part2/week16/chw/Math_1138.java
@@ -1,11 +1,31 @@
 package part2.week16.chw;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Math_1138 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int cnt = sc.nextInt();
+		int[] arr = new int[cnt];
+		for(int i = 0; i < cnt; i++) {
+			 int num = sc.nextInt();
+			 if(i == 0) {
+				 arr[num] = 1; 
+			 }else if(num == 0) {
+				 arr[0] = cnt;
+			 }else {
+				 if(arr[num] > 0) {
+					 arr[cnt-num] = i + 1;
+				 }else {
+					 arr[num] = i + 1;
+				 }
+			 }
+		}
+		for(int i : arr) {
+			System.out.println(i);
+		}
 		sc.close();
 	}
 }
+
