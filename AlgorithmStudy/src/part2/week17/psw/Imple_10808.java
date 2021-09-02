e57b78f622125a43a11da23a6e2d4114f98a6eb4
Ia7756 <gramian7756@gmail.com>
2021-09-02 13:12:55 +0900

    알파벳 개수

--- AlgorithmStudy/src/part2/week17/psw/Imple_10808.java
+++ AlgorithmStudy/src/part2/week17/psw/Imple_10808.java
@@ -1,12 +1,18 @@
 package part2.week17.psw;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Imple_10808 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] answer = new int['z'-'a'+1];
 
+        for (char ch: sc.next().toCharArray()) {
+            answer[ch-'a']++;
+        }
 
+        System.out.println(Arrays.toString(answer).replaceAll("[\\],\\,,\\[]",""));
         sc.close();
     }
 }
