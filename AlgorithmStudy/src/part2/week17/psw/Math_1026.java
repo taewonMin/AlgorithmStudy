cb7fbce233ecdbf38af1c5368acf890b9dee852e
Ia7756 <gramian7756@gmail.com>
2021-09-03 10:49:52 +0900

    보물

--- AlgorithmStudy/src/part2/week17/psw/Math_1026.java
+++ AlgorithmStudy/src/part2/week17/psw/Math_1026.java
@@ -1,12 +1,25 @@
 package part2.week17.psw;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Math_1026 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
+        final int N = sc.nextInt();
+        int answer = 0;
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>((o1, o2) -> -Integer.compare(o1,o2));
+        for (int i = 0; i < N; i++) {
+            a.add(sc.nextInt());
+        }
+        for (int i = 0; i < N; i++) {
+            b.add(sc.nextInt());
+        }
+        for (int i = 0; i < N; i++) {
+            answer +=a .poll()*b.poll();
+        }
+        System.out.println(answer);
         sc.close();
     }
 }
