35adee9d1ff651a55c72ec17d2fe2d76f14cf035
smartoc <smartoc@210.223.226.83>
2021-09-08 21:00:02 +0900

    17주차 커밋

--- AlgorithmStudy/src/part2/week17/mtw/Imple_10808.java
+++ AlgorithmStudy/src/part2/week17/mtw/Imple_10808.java
@@ -6,7 +6,17 @@ public class Imple_10808 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+        int[] alphabet = new int[26];
+        String str = sc.nextLine();
+        for(int i=0; i<str.length(); i++) {
+        	int cnt = str.charAt(i) - 97;
+        	alphabet[cnt]++;
+        }
+        
+        for(int i : alphabet) {
+        	System.out.print(i+" ");
+        }
+        
         sc.close();
     }
 }
--- AlgorithmStudy/src/part2/week17/mtw/Math_1026.java
+++ AlgorithmStudy/src/part2/week17/mtw/Math_1026.java
@@ -1,11 +1,30 @@
 package part2.week17.mtw;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Math_1026 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        Integer[] B = new Integer[N];
+        for(int i=0; i<N; i++) {
+        	A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        for(int i=0; i<N; i++) {
+        	B[i] = sc.nextInt();
+        }
+        Arrays.sort(B, (a,b) -> a.compareTo(b)*-1);
+        
+        int sum = 0;
+        for(int i=0; i<N; i++) {
+        	sum += A[i]*B[i];
+        }
+        
+        System.out.println(sum);
 
         sc.close();
     }
