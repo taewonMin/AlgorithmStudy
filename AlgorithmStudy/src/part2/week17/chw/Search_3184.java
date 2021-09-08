58664eebf163c44b64c82fee1c5570b67210e354
hyeonwoo <tifhs43@naver.com>
2021-09-08 12:09:34 +0900

    양 커밋

--- AlgorithmStudy/src/part2/week17/chw/Search_3184.java
+++ AlgorithmStudy/src/part2/week17/chw/Search_3184.java
@@ -1,88 +1,67 @@
 package part2.week17.chw;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Search_3184 {
+	static int sheep = 0;
+	static int wolf = 0;
+	static int sheepCnt = 0;
+    static int wolfCnt = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int width = sc.nextInt();
         int height = sc.nextInt();
-        int sheep = 0;
-        int wolf = 0;
+        String[][] map = new String[width][height];
+        boolean[][] visited = new boolean[width][height];
         
-        String[][] map = new String[height][width];
-        boolean[][] visited = new boolean[height][width];
-        
-        for(int i = 0; i < height; i++) {
+        for(int i = 0; i < width; i++) {
     		map[i] = sc.next().split("");
         }
-        dfs(visited, map, true);
+        sc.close();
         
-        
+        for(int i = 0; i < width; i++) {
+        	for(int j = 0; j < height; j++) {
+        		if(!visited[i][j] && !"#".equals(map[i][j])) {
+        			dfs(visited, map, j, i);
+        			if(sheepCnt > wolfCnt) {
+        				sheep += sheepCnt;
+        			}else {
+        				wolf += wolfCnt;
+        			}
+        			sheepCnt = 0;
+        			wolfCnt = 0;
+        		}
+        	}
+        }
+        System.out.println(sheep + " " + wolf);
     }
     
-    static void dfs(boolean[][] visited, String[][] map, boolean flag) {
-    	Stack<Integer[]> stack = new Stack<Integer[]>();
-    	Integer[] idx = checkCoordinate(map);
-    	stack.push(idx);
-    	visited[idx[0]][idx[1]] = true;
-    	while(!stack.isEmpty()) {
-    		Integer[] idx2 = stack.peek();
-    		flag = false;
-    		
-    		for(int i = 0; i < map.length; i++) {
-    			for(int j = 0; j < map[i].length; j++) {
-    				if(!visited[i][j]) {
-    					Integer[] idx3 = {i,j};
-    					stack.push(idx3);
-    					visited[i][j] = true;
-    					flag = true;
-    					break;
-    				}else if("#".equals(map[i][j])) {
-    					
-    					if(i == map.length-1 || j == 0) {
-    						flag = true;
-    						break;
-    					}
-    					
-    					if(!"#".equals(map[i+1][j-1])) {
-    						Integer[] idx3 = {i+1,j-1};
-        					stack.push(idx3);
-        					visited[i+1][j-1] = true;
-        					flag = true;
-        					break;
-    					}else {
-    						flag = true;
-    						break;
-    					}
-    				}
-    			}
-    			if(flag) break;
-    		}
-    		if(!flag) {
-    			System.out.println(stack.peek());
-    			stack.pop();
-    			
-    		}
-    		
-    		
+    static void dfs(boolean[][] visited, String[][] map, int x, int y) {
+    	if("v".equals(map[y][x])) {
+    		wolfCnt++;
+    	}else if("o".equals(map[y][x])) {
+    		sheepCnt++;
     	}
-    	
-    	
-	}
-    static Integer[] checkCoordinate(String[][] map) {
-    	for(int i = 0; i < map.length; i++) {
-    		for(int j = 0; j < map[i].length; j++) {
-    			if(!"#".equals(map[i][j])) {
-    				Integer[] idx = {i,j};
-    				return idx;
-    			}
-    		}
+    	visited[y][x] = true;
+    	for(int i = 0; i < 4; i++) {
+			int[] coordinate = checkDirection(visited, map, x, y);
+			if(coordinate != null) {
+				dfs(visited, map, coordinate[1], coordinate[0]);
+			}
+    	}
+    }
+    
+    static int[] checkDirection(boolean[][] visited, String[][] map, int x, int y) {
+    	if(y - 1 > -1 && !visited[y-1][x] && !"#".equals(map[y-1][x])) {
+    		return new int[] {y-1, x};
+    	}else if(y + 1 < map.length && !visited[y+1][x] && !"#".equals(map[y+1][x])) {
+    		return new int[] {y+1, x};
+    	}else if(x - 1 > -1 && !visited[y][x-1] && !"#".equals(map[y][x-1])) {
+    		return new int[] {y, x-1};
+    	}else if(x + 1 < map[y].length && !visited[y][x+1] && !"#".equals(map[y][x+1])) {
+    		return new int[] {y, x+1};
     	}
     	return null;
     }
 }
-
-    
