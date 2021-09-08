78c5c237c7184a7f1fa48988deb37ed0a9959eed
stops <stopsilver212@gmail.com>
2021-09-08 20:53:58 +0900

    트리 순회(미완)

--- AlgorithmStudy/src/part2/week17/jje/Tree_1991.java
+++ AlgorithmStudy/src/part2/week17/jje/Tree_1991.java
@@ -1,12 +1,39 @@
 package part2.week17.jje;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Tree_1991 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<String, List<String>> treeMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<String> numList = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String key = st.nextToken();
+            numList.add(st.nextToken());
+            numList.add(st.nextToken());
+            treeMap.put(key, numList);
+        }
+
+        leftTree(treeMap);
 
 
         sc.close();
+    }
+
+    public static void leftTree(Map<String, List<String>> treeMap){
+        for(String key : treeMap.keySet()){
+            System.out.println(key);
+            List<String> numList = treeMap.get(key);
+            if(!".".equals(numList.get(0))){
+                System.out.println(numList.get(0));
+            }
+
+        }
     }
 }
