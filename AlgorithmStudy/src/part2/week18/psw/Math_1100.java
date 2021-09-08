217b95424a9f1c37cea859f0b0e4600e94998baf
Ia7756 <gramian7756@gmail.com>
2021-09-08 23:55:12 +0900

    하얀 말

--- AlgorithmStudy/src/part2/week18/psw/Math_1100.java
+++ AlgorithmStudy/src/part2/week18/psw/Math_1100.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class Math_1100 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int answer = 0;
 
+        int i = 0;
+        for (int j = 0; j < 8; j++) {
+            for (String horse: sc.nextLine().split("")) {
+                if((i+j)%2 == 0 &&  "F".equals(horse)) answer++;
+                i++;
+            }
+        }
+
+        System.out.println(answer);
         sc.close();
-
     }
 }
