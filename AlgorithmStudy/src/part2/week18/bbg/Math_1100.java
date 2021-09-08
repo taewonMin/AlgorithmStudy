45d2d711998d2b1d1751f6c90634bae7f52493f7
Ia7756 <gramian7756@gmail.com>
2021-09-08 21:48:18 +0900

    18 주

---
+++ AlgorithmStudy/src/part2/week18/bbg/Math_1100.java
@@ -0,0 +1,12 @@
+package part2.week18.bbg;
+
+import java.util.Scanner;
+
+public class Math_1100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+}
