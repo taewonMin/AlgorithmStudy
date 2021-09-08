785c50dd68e6501d68c0c760c769c15b3c4bbc3e
hyeonwoo <tifhs43@naver.com>
2021-09-08 19:13:41 +0900

    보물, 알파벳 커밋

--- AlgorithmStudy/src/part2/week17/chw/Imple_10808.java
+++ AlgorithmStudy/src/part2/week17/chw/Imple_10808.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Imple_10808 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
+        int[] arr = new int[26];
+        char[] input = sc.next().toCharArray();
+        for(char i : input) {
+        	arr[i - 97] += 1;
+        } 
+        for(int i : arr) {
+        	System.out.println(i);
+        }
         sc.close();
     }
 }
--- AlgorithmStudy/src/part2/week17/chw/Math_1026.java
+++ AlgorithmStudy/src/part2/week17/chw/Math_1026.java
@@ -1,12 +1,30 @@
 package part2.week17.chw;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Math_1026 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
+        
+        int cnt = sc.nextInt();
+        int result = 0;
+        Integer[] firArr = new Integer[cnt];
+        int[] secArr = new int[cnt];
+        for(int i = 0; i < cnt; i++) {
+        	firArr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < cnt; i++) {
+        	secArr[i] = sc.nextInt();
+        }
+        Arrays.sort(firArr, Collections.reverseOrder());
+        Arrays.sort(secArr);
+        
+        for(int i =0; i < cnt; i++) {
+        	result += firArr[i] * secArr[i];
+        }
+        System.out.println(result);
         sc.close();
     }
 }
