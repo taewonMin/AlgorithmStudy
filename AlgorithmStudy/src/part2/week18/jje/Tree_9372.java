9c1bea3d5ec15f44f155f5db8daa7f4b0bc98912
Jieun Jeong <stopsilver@kaoni.com>
2021-09-14 18:50:57 +0900

    상근이의 여행

--- AlgorithmStudy/src/part2/week18/jje/Tree_9372.java
+++ AlgorithmStudy/src/part2/week18/jje/Tree_9372.java
@@ -6,6 +6,18 @@ public class Tree_9372 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //링형..?
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for (int j = 0; j < m; j++) {
+                sc.nextInt();
+                sc.nextInt();
+            }
+            System.out.println(n-1);
+        }
         sc.close();
     }
 }
