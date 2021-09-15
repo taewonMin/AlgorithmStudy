0d6c7bc11e94a63a0a34e10d8d754597a63cbbed
spring531 <sprng_is_coming_531@naver.com>
2021-09-15 17:52:08 +0900

    상근이의 여행

--- AlgorithmStudy/src/part2/week18/phk/Tree_9372.java
+++ AlgorithmStudy/src/part2/week18/phk/Tree_9372.java
@@ -2,9 +2,21 @@ package part2.week18.phk;
 
 import java.util.Scanner;
 
+// 216444kb	968ms
 public class Tree_9372 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for (int j = 0; j < m; j++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+            }
+            System.out.println(n-1);
+        }
 
         sc.close();
     }
