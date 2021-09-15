827adbe8f065c0244cb44982f6449abafef1a8ea
gilbeee <7mini96@naver.com>
2021-09-15 21:01:17 +0900

    하얀칸

--- AlgorithmStudy/src/part2/week18/bbg/Math_1100.java
+++ AlgorithmStudy/src/part2/week18/bbg/Math_1100.java
@@ -1,12 +1,21 @@
 package part2.week18.bbg;
 
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/* 하얀칸 12828kb 124ms */
 public class Math_1100 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        for(int x = 0 ; x < 8 ; x++) { // line
+        	char[] arr = br.readLine().toCharArray();
+        	for(int y = 0 ; y < arr.length ; y++) {
+        		if((x%2 == 0 && y%2 == 0)||x%2 == 1 && y%2 == 1) if(arr[y] == 'F') cnt++;
+        	}
+        }
+        System.out.println(cnt);
+        br.close();
 
     }
 }
