32fdff95cebe880c84ae8907b0ff54cfe2987c13
spring531 <sprng_is_coming_531@naver.com>
2021-09-16 17:02:44 +0900

    색종이

--- AlgorithmStudy/src/part2/week19/phk/Imple_2563.java
+++ AlgorithmStudy/src/part2/week19/phk/Imple_2563.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class Imple_2563 {
     public static void main(String[] args) {
+        int[][] page = new int[100][100];
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    page[j][k]++;
+                }
+            }
+        }
         sc.close();
+
+        int d = 0;
+        for (int i = 0; i < page.length; i++) {
+            for (int j = 0; j < page[i].length; j++) {
+                if(page[i][j] != 0) d++;
+            }
+        }
+        System.out.println(d);
     }
 }
