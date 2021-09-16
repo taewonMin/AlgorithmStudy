8efc5cc3be33ad4aa1fab8161275c185e093977a
hyeonwoo <tifhs43@naver.com>
2021-09-16 18:17:07 +0900

    19주차 커밋

--- AlgorithmStudy/src/part2/week19/chw/Dynamic_17212.java
+++ AlgorithmStudy/src/part2/week19/chw/Dynamic_17212.java
@@ -3,9 +3,29 @@ package part2.week19.chw;
 import java.util.Scanner;
 
 public class Dynamic_17212 {
+	static int[] coinArr = {7, 5, 2, 1};
+	static int result = 0;
+	static int idx = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        calculator(sc.nextInt());
+        System.out.println(result);
         sc.close();
+    }
+    static void calculator(int input) {
+    	if(input != 0){
+    		if(input > 10 && input % 7 == 3) {
+    			result += input / coinArr[idx] - 1;
+    			input = 10;
+    		}else if(input == 10){
+    			result+=2;
+    			return;
+    		}else {
+    			result += input / coinArr[idx];
+    			input = input % coinArr[idx];
+    		}
+			idx++;
+			calculator(input);
+    	}
     }
 }
--- AlgorithmStudy/src/part2/week19/chw/Imple_2563.java
+++ AlgorithmStudy/src/part2/week19/chw/Imple_2563.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class Imple_2563 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int cnt = sc.nextInt();
+        int[][] arr = new int[100][100];
+        int sum = 0;
+        for(int i = 0; i < cnt; i++) {
+        	int x = sc.nextInt();
+        	int y = sc.nextInt();
+        	for(int j = y; j < y+10; j++) {
+        		for(int k = x; k < x+10; k++) {
+        			arr[j][k] = 1;
+        		}
+        	}
+        }
+        for(int[] ar : arr) {
+        	for(int a : ar) {
+        		if(a == 1) sum++;
+        	}
+        }
+        System.out.println(sum);
         sc.close();
     }
 }
