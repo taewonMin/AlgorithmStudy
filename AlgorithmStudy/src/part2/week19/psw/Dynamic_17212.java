f8ecf14e4b5d6fc686622ffa3b33054a613ccbf3
Ia7756 <gramian7756@gmail.com>
2021-09-26 14:49:10 +0900

    토끼

--- AlgorithmStudy/src/part2/week19/psw/Dynamic_17212.java
+++ AlgorithmStudy/src/part2/week19/psw/Dynamic_17212.java
@@ -3,9 +3,23 @@ package part2.week19.psw;
 import java.util.Scanner;
 
 public class Dynamic_17212 {
+    static int n = 0;
+    static int answer = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        n = sc.nextInt();
         sc.close();
+
+        int idx = n/7;
+        while (idx-- > -1){
+            int cnt = idx+1;
+            if(n > cnt*7){
+                int remain = n-cnt*7;
+                cnt += remain/5 + (remain%5)/2 + (remain%5)%2;
+            }
+            if (answer == 0) answer = cnt;
+            answer = answer > cnt ? cnt : answer;
+        }
+        System.out.println(answer);
     }
 }
