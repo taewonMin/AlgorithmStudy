fe0366d280f289bcf10136e9d6179871e547ce04
spring531 <sprng_is_coming_531@naver.com>
2021-09-27 16:11:43 +0900

    달토끼

--- AlgorithmStudy/src/part2/week19/phk/Dynamic_17212.java
+++ AlgorithmStudy/src/part2/week19/phk/Dynamic_17212.java
@@ -5,7 +5,29 @@ import java.util.Scanner;
 public class Dynamic_17212 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int result = n/7;
+        switch( n%7 ){
+            case 1: 
+            case 2: 
+            case 5:
+                result += 1;
+                break;  
+            case 4: 
+            case 6: 
+                result += 2;
+                break;  
+            case 3: 
+                result += (n < 7 ? 2 : 1); 
+                break;
+            default:
+                break;
+            
+            
+        }
 
+        System.out.println(result);
         sc.close();
     }
 }
