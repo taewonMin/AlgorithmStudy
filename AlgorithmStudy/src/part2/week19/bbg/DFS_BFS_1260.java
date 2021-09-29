fcd6ba3e5af08b5dfe772a2efc0680b094064230
gilbeee <7mini96@naver.com>
2021-09-29 21:04:16 +0900

    0929

--- AlgorithmStudy/src/part2/week19/bbg/DFS_BFS_1260.java
+++ AlgorithmStudy/src/part2/week19/bbg/DFS_BFS_1260.java
@@ -1,11 +1,51 @@
 package part2.week19.bbg;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
+/*
+ * DFS = 깊이우선 = 스택이나 재귀함수 이용
+ * 
+ * BFS = 너비우선 = 큐 이용
+ * */
 
 public class DFS_BFS_1260 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int N = sc.nextInt(); // 점 개수
+        int M = sc.nextInt(); // 선 개수
+        int V = sc.nextInt(); // 시작 번호
+        int[][] loc = new int[N][2]; // 점 위치 저장
+        boolean[] visited = new boolean[N]; // 초기값 false, 방문 시 true
+        LinkedList<Integer>[] listArr = new LinkedList[N+1];
+        
+        for(int cnt = 0 ; cnt < M ; cnt++) {
+        	listArr[cnt] = new LinkedList<>();
+        	// 선이 연결하는 두 점의 번호
+        	int fir = sc.nextInt();
+        	int sec = sc.nextInt();
+//        	listArr[fir];
+        }
+        
+        
         sc.close();
     }
+    
+    void bfs(int start, boolean[] arr) {
+    	Queue<Integer> q = new LinkedList<>();
+    	q.add(start);
+    	arr[start] = true;
+    	while(!q.isEmpty()) {
+    		int x = q.poll();
+    		System.out.println(x);
+//    		for(int i=0 ; i < ) {
+    			
+//    		}
+    	}
+    }
+    
+    
 }
--- AlgorithmStudy/src/part2/week19/bbg/Dynamic_17212.java
+++ AlgorithmStudy/src/part2/week19/bbg/Dynamic_17212.java
@@ -1,11 +1,45 @@
 package part2.week19.bbg;
 
 import java.util.Scanner;
-
+/*달토끼 */
 public class Dynamic_17212 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int[] coins = {7, 5, 2, 1};
+        int N = sc.nextInt();
+        int N2 = N;
         sc.close();
+        int idx = 0;
+        int cnt = 0;
+        /*
+         *  최대공약수? 최소공배수?
+         *  인수?
+         *  7 5 2 1로 쪼갤 수 있는 경우의 수 중 가장 작은 수 리턴
+         *  분기해서 원래 7의 개수 -1개로 조회..? 큰수일수록 안될거같은데
+         * */
+        int seven = 0;
+        while(true) {
+        	cnt += N/coins[idx];
+        	if(idx == 0) seven = cnt; // 제일 큰 수의 cnt만 담기
+        	N = N%coins[idx++];
+        	
+        	if(N == 0) break;
+        }
+        
+        idx = 0;
+        int cnt2 = 0;
+        while(true) {
+        	if(cnt2 == 0) {
+        		cnt2 += seven-1;
+        		N2 = N2-(coins[idx]*(seven-1));
+        	}else {
+        		cnt2 += N2/coins[idx];
+        		N2 = N2%coins[idx];
+        		if(N2 == 0) break;
+        	}
+        	idx++;
+        }
+        
+        System.out.println(cnt > cnt2 ? cnt2 : cnt);
     }
 }
