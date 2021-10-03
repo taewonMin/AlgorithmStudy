513809af163f516e94357842bd2eb811c0688104
smarto <mtw9411@naver.com>
2021-10-03 11:24:11 +0900

    commit

---
+++ AlgorithmStudy/src/part2/week19/mtw/Hide_Seek_1697.java
@@ -0,0 +1,16 @@
+package part2.week19.mtw;
+
+import java.util.Scanner;
+
+public class Hide_Seek_1697{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        
+
+        sc.close();
+    }
+}
---
+++ AlgorithmStudy/src/part2/week19/mtw/Tomato_7569.java
@@ -0,0 +1,13 @@
+package part2.week19.mtw;
+
+import java.util.Scanner;
+
+public class Tomato_7569 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
