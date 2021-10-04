451c611d882106231f61a2916687d049cb51a97e
smarto <mtw9411@naver.com>
2021-10-05 02:15:05 +0900

    하노이 탑

--- AlgorithmStudy/src/part2/week20/mtw/Recursion_11729.java
+++ AlgorithmStudy/src/part2/week20/mtw/Recursion_11729.java
@@ -1,11 +1,33 @@
 package part2.week20.mtw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Recursion_11729 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sc.close();
+        int N = Integer.parseInt(br.readLine());
+        bw.write((int)(Math.pow(2, N)-1) + "\n");
+        hanoi(N, 1, 2, 3);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    static void hanoi(int n, int from, int via, int to) throws IOException {
+        if(n==1){
+            bw.write(from + " " + to + "\n");
+        }else{
+            hanoi(n-1, from, to, via);
+            bw.write(from + " " + to);
+            bw.newLine();
+            hanoi(n-1, via, from, to);
+        }
     }
 }
