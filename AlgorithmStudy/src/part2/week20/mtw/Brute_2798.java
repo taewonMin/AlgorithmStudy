3f6530637692c43942ca53c708985f4972dd1bb2
smarto <mtw9411@naver.com>
2021-10-05 02:38:00 +0900

    commit

--- AlgorithmStudy/src/part2/week20/mtw/Brute_2798.java
+++ AlgorithmStudy/src/part2/week20/mtw/Brute_2798.java
@@ -1,11 +1,23 @@
 package part2.week20.mtw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Brute_2798 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sc.close();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            st.nextToken();
+        }
+        
+        br.close();
     }
 }
