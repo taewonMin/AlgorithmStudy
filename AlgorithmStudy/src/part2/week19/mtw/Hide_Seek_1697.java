87c80de46154d5979eca911450ad1debc712fe6e
smarto <mtw9411@naver.com>
2021-10-06 02:22:50 +0900

    숨바꼭질

--- AlgorithmStudy/src/part2/week19/mtw/Hide_Seek_1697.java
+++ AlgorithmStudy/src/part2/week19/mtw/Hide_Seek_1697.java
@@ -1,16 +1,52 @@
 package part2.week19.mtw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Hide_Seek_1697{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static boolean[] visited = new boolean[100001];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        String[] input = br.readLine().split(" ");
 
-        
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
 
-        sc.close();
+        bfs(N, K);
+
+        br.close();
+    }
+
+    public static void bfs(int N, int K){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{N, 0});
+        visited[N] = true;
+        while(!queue.isEmpty()){
+            int[] next = queue.poll();
+            if(next[0] == K){
+                System.out.println(next[1]);
+                break;
+            }
+
+            if(next[0] > 0 && visited[next[0]-1] == false){
+                queue.offer(new int[]{next[0]-1, next[1]+1});
+                visited[next[0]-1] = true;
+            }
+            if(next[0] < K){
+                if(next[0]<100000 && visited[next[0]+1] == false){
+                    queue.offer(new int[]{next[0]+1, next[1]+1});
+                    visited[next[0]+1] = true;
+                }
+                if(next[0]<50001 && visited[next[0]*2] == false){
+                    queue.offer(new int[]{next[0]*2, next[1]+1});
+                    visited[next[0]*2] = true;
+                }
+            }
+            
+        }
     }
 }
