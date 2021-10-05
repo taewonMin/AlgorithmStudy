e5bbce525464e5db31c94844b6ebd2cb76e73e24
smarto <mtw9411@naver.com>
2021-10-06 01:34:45 +0900

    블랙잭

--- AlgorithmStudy/src/part2/week20/mtw/Brute_2798.java
+++ AlgorithmStudy/src/part2/week20/mtw/Brute_2798.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Brute_2798 {
+    public static int[] card;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -13,11 +14,36 @@ public class Brute_2798 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        card = new int[N];
         st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()){
-            st.nextToken();
+        int len = st.countTokens();
+        for(int i=0; i<len; i++){
+            card[i] = Integer.parseInt(st.nextToken());
         }
         
+        System.out.println(blackJack(M));
+        
         br.close();
+    }
+
+    public static int blackJack(int M){
+        int max = 0;
+        for(int i=0; i<card.length; i++){
+            if(card[i] < M){
+                for(int j=i+1; j<card.length; j++){
+                    if(card[i] + card[j] < M){
+                        for(int k=j+1; k<card.length; k++){
+                            int sum = card[i] + card[j] + card[k];
+                            if(sum == M){
+                                return sum;
+                            }else if(sum < M && sum > max){
+                                max = sum;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return max;
     }
 }
