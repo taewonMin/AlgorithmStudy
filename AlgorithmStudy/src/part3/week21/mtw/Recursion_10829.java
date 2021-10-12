a51c327a7268e9f4aec97472c3b276f12dec2ee1
smarto <mtw9411@naver.com>
2021-10-13 04:33:10 +0900

    이진법

--- AlgorithmStudy/src/part3/week21/mtw/Recursion_10829.java
+++ AlgorithmStudy/src/part3/week21/mtw/Recursion_10829.java
@@ -6,6 +6,15 @@ public class Recursion_10829 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        long N = sc.nextLong();
+        System.out.println(binaryNumber(N));
+
         sc.close();
+    }
+
+    public static String binaryNumber(long N){
+        if(N==1) return String.valueOf(N);
+        
+        return binaryNumber(N/2) + String.valueOf(N%2);
     }
 }
