cacec58d1f77942d7cf03c11f66fb0bbc0958b15
hyeonwoo <tifhs43@naver.com>
2021-10-13 20:40:06 +0900

    21주차 커밋

---
+++ AlgorithmStudy/src/part3/week21/chw/Brute_1436.java
@@ -0,0 +1,18 @@
+package part3.week21.chw;
+
+import java.util.Scanner;
+
+public class Brute_1436 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        sc.close();
+        int cnt = 0;
+        int startNum = 665;
+        while(input != cnt) {
+        	startNum++;
+        	if((startNum+"").contains("666")) cnt++;
+        }
+        System.out.println(startNum);
+    }
+}
