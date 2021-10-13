37380e8f5c863ad65b6232f533e168a7dbb16c07
Jieun Jeong <stopsilver@kaoni.com>
2021-10-13 15:56:29 +0900

    이진법

--- AlgorithmStudy/src/part3/week21/jje/Recursion_10829.java
+++ AlgorithmStudy/src/part3/week21/jje/Recursion_10829.java
@@ -1,11 +1,33 @@
 package part3.week21.jje;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Recursion_10829 {
+
+    static long result = 0;
+    static Stack<Integer> stack = new Stack<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        recursion(n);
+        int stackSize = stack.size();
+        for (int i = 0; i < stackSize; i++) {
+            System.out.print(stack.pop());
+        }
 
         sc.close();
+    }
+
+
+    static void recursion (long n) {
+        result = n / 2;
+        stack.push((int)n % 2);
+        if (result > 0) {
+            recursion(result);
+        } else {
+            return;
+        }
     }
 }
