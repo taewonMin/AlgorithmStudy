621e0972037d47b33ff5b831557e8f87a56b7a2a
hyeonwoo <tifhs43@naver.com>
2021-10-13 20:39:27 +0900

    21주차 커밋

--- AlgorithmStudy/src/part3/week21/chw/Brute_2231.java
+++ AlgorithmStudy/src/part3/week21/chw/Brute_2231.java
@@ -5,7 +5,25 @@ import java.util.Scanner;
 public class Brute_2231 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int input = sc.nextInt();
+        int startNum = 0;
+        int result = 0;
+        
+        while(startNum != input) {
+        	startNum++;
+        	int sum = startNum;
+        	String[] bunhae = (startNum+"").split("");
+        	for(String a : bunhae) {
+        		sum += Integer.parseInt(a);
+        	}
+        	if(input == sum) {
+        		result = startNum;
+        		break;
+        	}
+        	
+        }
+        System.out.println(result);
+        
         sc.close();
     }
 }
--- AlgorithmStudy/src/part3/week21/chw/Recursion_10829.java
+++ AlgorithmStudy/src/part3/week21/chw/Recursion_10829.java
@@ -3,9 +3,27 @@ package part3.week21.chw;
 import java.util.Scanner;
 
 public class Recursion_10829 {
+	static String result = "";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        convert(sc.nextLong());
+        System.out.println(result);
         sc.close();
     }
+    static void convert(long input) {
+    	if(input == 0) return;
+    	result = (input % 2 == 0 ? 0 : 1) + result;
+    	input = input / 2;
+    	convert(input);
+    }
 }
+/*
+
+53 -> 26 ... 1
+26 -> 13 ... 0
+13 -> 6  ... 1
+6  -> 3  ... 0
+3  -> 1  ... 1
+1  -> 0  ... 1
+
+*/
