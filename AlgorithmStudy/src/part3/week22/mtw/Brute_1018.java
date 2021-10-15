37b1938c225ac0e574dfd99dcd7d6b1378648b2b
smarto <mtw9411@naver.com>
2021-10-16 00:25:53 +0900

    체스판 칠하기

--- AlgorithmStudy/src/part3/week22/mtw/Brute_1018.java
+++ AlgorithmStudy/src/part3/week22/mtw/Brute_1018.java
@@ -1,11 +1,69 @@
 package part3.week22.mtw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Brute_1018 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Brute_1018{
+    public static String[] chess;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sc.close();
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+
+        chess = new String[N];
+        for(int i=0; i<N; i++){
+            chess[i] = br.readLine();
+        }
+
+        System.out.println(selectChessBoard(N, M));
+
+        br.close();
+    }
+
+    // 체스판 고르기
+    public static int selectChessBoard(int N, int M){
+        int min = N*M;
+        for(int i=0; i<N-7; i++){
+            for(int j=0; j<M-7; j++){
+                int cnt = redrawChess(i, j);
+                if(cnt == 0) return 0;
+                if(cnt < min){
+                    min = cnt;
+                }
+            }
+        }
+        return min;
+    }
+
+    // 색깔 칠한 개수 구하기
+    public static int redrawChess(int x, int y){
+        int startWCnt = 0;
+        int startBCnt = 0;
+        for(int i=x; i<x+8; i++){
+            for(int j=y; j<y+8; j++){
+                if((i-x) % 2 == 0){ // 짝수행
+                    if((j-y) % 2 == 0){ // 짝수열
+                        if(chess[i].charAt(j) == 'B') startWCnt++;
+                        else startBCnt++;
+                    }else{ // 홀수열
+                        if(chess[i].charAt(j) == 'W') startWCnt++;
+                        else startBCnt++;
+                    }
+                }else{ // 홀수행
+                    if((j-y) % 2 == 0){ // 짝수열
+                        if(chess[i].charAt(j) == 'W') startWCnt++;
+                        else startBCnt++;
+                    }else{ // 홀수열
+                        if(chess[i].charAt(j) == 'B') startWCnt++;
+                        else startBCnt++;
+                    }
+                }
+                
+            }
+        }
+        return Math.min(startWCnt, startBCnt);
     }
 }
