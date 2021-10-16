11cc57ee76730192ef5fea8c4591b1700d88b490
Ia7756 <gramian7756@gmail.com>
2021-10-16 16:34:49 +0900

    덩치

--- AlgorithmStudy/src/part3/week22/psw/Brute_7568.java
+++ AlgorithmStudy/src/part3/week22/psw/Brute_7568.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class Brute_7568 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        int[][] mans = new int[N][];
+        for (int i = 0; i < N; i++) {
+            mans[i] = new int[]{sc.nextInt(),sc.nextInt()};
+        }
+
+        for (int i = 0; i < N; i++) {
+            int answer = 1;
+            for (int j = 0; j < N; j++) {
+                if (i==j) continue;
+                if (mans[i][0] < mans[j][0] && mans[i][1] < mans[j][1]) answer++;
+            }
+            System.out.print(answer+" ");
+        }
 
         sc.close();
     }
