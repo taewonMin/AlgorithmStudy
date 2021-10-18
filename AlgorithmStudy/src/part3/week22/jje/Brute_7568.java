764553d4b1a3673849bed368aaf150f1cec61cd4
Jieun Jeong <stopsilver@kaoni.com>
2021-10-18 12:36:16 +0900

    덩치

--- AlgorithmStudy/src/part3/week22/jje/Brute_7568.java
+++ AlgorithmStudy/src/part3/week22/jje/Brute_7568.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Brute_7568 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] weight = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            weight[i][0] = sc.nextInt();
+            weight[i][1] = sc.nextInt();
+        }
 
+
+        for (int i = 0; i < n; i++) {
+            int result = 1;
+            for (int j = 0; j < n; j++) {
+                if(weight[i][0] < weight[j][0] && weight[i][1] < weight[j][1]) result++;
+            }
+            System.out.print(result+" ");
+        }
         sc.close();
     }
 }
