2b6080ffa3103bbebf5ef3d5ccd26f426e8281de
hyeonwoo <tifhs43@naver.com>
2021-10-19 20:02:35 +0900

    22주차 커밋

--- AlgorithmStudy/src/part3/week22/chw/Brute_1436.java
+++ AlgorithmStudy/src/part3/week22/chw/Brute_1436.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Brute_1436 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int input = sc.nextInt();
         sc.close();
+        int cnt = 0;
+        int startNum = 665;
+        while(input != cnt) {
+        	startNum++;
+        	if((startNum+"").contains("666")) cnt++;
+        }
+        System.out.println(startNum);
     }
 }
--- AlgorithmStudy/src/part3/week22/chw/Brute_7568.java
+++ AlgorithmStudy/src/part3/week22/chw/Brute_7568.java
@@ -2,10 +2,24 @@ package part3.week22.chw;
 
 import java.util.Scanner;
 
-public class Brute_7568 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
-    }
+public class Brute_7568{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		int[][] arr = new int[cnt][2];
+		for(int i = 0; i < cnt; i++){
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		for(int i = 0; i < cnt; i++){
+			int count = 1;
+			for(int j = 0; j < cnt; j++){
+				if(i != j && arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]){
+					count++;
+				}
+			}
+			System.out.print(count + " ");
+		}
+		sc.close();
+	}
 }
