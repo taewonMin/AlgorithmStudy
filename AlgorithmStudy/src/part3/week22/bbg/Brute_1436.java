e5c37787e8eabaa1e0b11c168e9b8a299d15ed9b
gilbeee <7mini96@naver.com>
2021-10-20 20:57:40 +0900

    영화감독

--- AlgorithmStudy/src/part3/week22/bbg/Brute_1436.java
+++ AlgorithmStudy/src/part3/week22/bbg/Brute_1436.java
@@ -1,11 +1,27 @@
 package part3.week22.bbg;
 
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+/*영화감독 메모리초과*/
 public class Brute_1436 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		int count = 0;
+		int result = 665;
+		StringBuffer sb = new StringBuffer();
+		String pattern = "\\d*(666)+\\d*";
+		while(count < N) {
+			result++;
+			sb.delete(0, sb.length());
+			sb.append(result);
+			if(Pattern.matches(pattern, sb)) { 
+				count++;
+			}
+		}
+		System.out.println(result);
+	}
 }
