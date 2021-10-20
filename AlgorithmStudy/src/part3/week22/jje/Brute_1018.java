f02a0087a4392dd6ab24abde468cdd72fd530c58
Jieun Jeong <stopsilver@kaoni.com>
2021-10-20 17:20:37 +0900

    체스판 다시 칠하기

--- AlgorithmStudy/src/part3/week22/jje/Brute_1018.java
+++ AlgorithmStudy/src/part3/week22/jje/Brute_1018.java
@@ -1,11 +1,64 @@
 package part3.week22.jje;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Brute_1018 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String first = br.readLine();
+        int n = Integer.parseInt(first.split(" ")[0]);
+        int m = Integer.parseInt(first.split(" ")[1]);
+        StringBuilder sb = new StringBuilder();
+        int result = 0;
+        sb.append(br.readLine());
+        for (int i = 1; i < m; i++) {
+            String nextRow = br.readLine();
+            for (int j = 0; j < n; j++) {
+                String con = String.valueOf(sb.charAt(n * i - (n - (j + 1))));
+                String cur = String.valueOf(nextRow.charAt(j));
 
-        sc.close();
+                if (j == n - 1 && con.equals(cur)) {
+                    result++;
+                    sb.append(String.valueOf(cur.equals("W")? "B" : "W"));
+                    continue;
+                }
+
+                if (j != n - 1 && !con.equals(cur)) {
+                    result++;
+                    sb.append(con);
+                    continue;
+                }
+                sb.append(cur);
+            }
+        }
+        System.out.println(result);
     }
 }
+
+/*
+8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+* */
+
+/*
+ 10 13
+ BBBBBBBBWBWBW
+ BBBBBBBBBWBWB
+ BBBBBBBBWBWBW
+ BBBBBBBBBWBWB
+ BBBBBBBBWBWBW
+ BBBBBBBBBWBWB
+ BBBBBBBBWBWBW
+ BBBBBBBBBWBWB
+ WWWWWWWWWWBWB
+ WWWWWWWWWWBWB
+ */
