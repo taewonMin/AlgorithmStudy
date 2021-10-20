1c9a2788e7fc2d6172957b88f4c2bacbece098fa
Jieun Jeong <stopsilver@kaoni.com>
2021-10-20 17:38:35 +0900

    영화감독 숌

--- AlgorithmStudy/src/part3/week22/jje/Brute_1436.java
+++ AlgorithmStudy/src/part3/week22/jje/Brute_1436.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class Brute_1436 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int movie = 666;
+        int add = 0;
 
+        while (true){
+            if(String.valueOf(movie).contains("666")){
+                add++;
+                if(add == n){
+                    break;
+                }
+            }
+            movie++;
+        }
+        System.out.println(movie);
         sc.close();
     }
 }
