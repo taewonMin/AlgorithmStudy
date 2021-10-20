e63e17c177edb672ef2252bdfe5ba73e39163f0e
Ia7756 <gramian7756@gmail.com>
2021-10-20 21:43:51 +0900

    23주차

---
+++ AlgorithmStudy/src/part3/week23/jje/Backtracking_15649.java
@@ -0,0 +1,11 @@
+package part3.week23.jje;
+
+import java.util.Scanner;
+
+public class Backtracking_15649 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
---
+++ AlgorithmStudy/src/part3/week23/phk/Sort_18870.java
@@ -0,0 +1,11 @@
+package part3.week23.phk;
+
+import java.util.Scanner;
+
+public class Sort_18870 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
