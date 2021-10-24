1fb888e988a35aba0fe6f04db3e9ef8c359c04e4
smarto <mtw9411@naver.com>
2021-10-24 19:58:44 +0900

    나이 정렬

--- AlgorithmStudy/src/part3/week23/mtw/Sort_10814.java
+++ AlgorithmStudy/src/part3/week23/mtw/Sort_10814.java
@@ -1,11 +1,51 @@
 package part3.week23.mtw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Sort_10814 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sc.close();
+        int N = Integer.parseInt(br.readLine());
+        Member[] memberList = new Member[N];
+
+        for(int i=0; i<N; i++){
+            String[] input = br.readLine().split(" ");
+            memberList[i] = new Member(Integer.parseInt(input[0]), input[1], i);
+        }
+
+        Arrays.sort(memberList, (a, b) -> {
+            int cnt = a.age.compareTo(b.age);
+            if(cnt != 0){
+                return cnt;
+            }
+            return a.order.compareTo(b.order);
+        });
+
+        for(Member member : memberList){
+            System.out.println(member.toString());
+        }
+
+        br.close();
+    }
+}
+
+class Member{
+    Integer age;
+    String name;
+    Integer order;
+
+    Member(int age, String name, int order){
+        this.age = age;
+        this.name = name;
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return this.age + " " + this.name;
     }
 }
