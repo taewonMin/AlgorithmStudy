88af0a2d4d12494694e05d2df8b918876f1f74a4
smarto <mtw9411@naver.com>
2021-10-24 20:29:00 +0900

    개미의 이동

--- AlgorithmStudy/src/part1/week6_sort/mtw/Sort_3221.java
+++ AlgorithmStudy/src/part1/week6_sort/mtw/Sort_3221.java
@@ -1,5 +1,24 @@
 package part1.week6_sort.mtw;
 
-public class Sort_3221 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+// 개미의 이동
+public class Sort_3221 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int L = Integer.parseInt(input[0]);
+        int T = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            String[] ant = br.readLine().split(" ");
+            
+        }
+
+        br.close();
+    }
 }
