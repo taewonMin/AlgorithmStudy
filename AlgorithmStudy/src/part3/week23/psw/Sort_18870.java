3b137990fc4860cd5e7037b5b6dd17af56b5d881
Ia7756 <gramian7756@gmail.com>
2021-10-27 20:24:17 +0900

    정렬 실패..

--- AlgorithmStudy/src/part3/week23/psw/Sort_18870.java
+++ AlgorithmStudy/src/part3/week23/psw/Sort_18870.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Sort_18870 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
         sc.close();
     }
 }
