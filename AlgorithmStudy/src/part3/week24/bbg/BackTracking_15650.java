470a56fefed27e9efaea508ef8e85ad7e7e54ae1
gilbeee <7mini96@naver.com>
2021-10-27 22:58:56 +0900

    24주차 패키지

---
+++ AlgorithmStudy/src/part3/week24/bbg/BackTracking_15650.java
@@ -0,0 +1,11 @@
+package part3.week24.bbg;
+
+import java.util.Scanner;
+
+public class BackTracking_15650 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
