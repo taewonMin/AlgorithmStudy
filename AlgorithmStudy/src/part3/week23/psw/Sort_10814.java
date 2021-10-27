a1351256a2d61a126cc32bfd905423a636bd00b9
Ia7756 <gramian7756@gmail.com>
2021-10-27 20:23:47 +0900

    정렬

--- AlgorithmStudy/src/part3/week23/psw/Sort_10814.java
+++ AlgorithmStudy/src/part3/week23/psw/Sort_10814.java
@@ -1,10 +1,25 @@
 package part3.week23.psw;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
-
+//156136kb	2720ms
 public class Sort_10814 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Object[]> queue = new PriorityQueue<Object[]>((x,y) -> {
+            if ((int)x[0]==(int)y[0]) return Integer.compare((int)x[1],(int)y[1]);
+            return Integer.compare((int)x[0],(int)y[0]);
+        });
+
+        for (int i = 0; i < n; i++) {
+            queue.add(new Object[]{ sc.nextInt(),i, sc.next()});
+        }
+
+        while (!queue.isEmpty()) {
+            Object[] item = queue.poll();
+            System.out.println((int)item[0] +" "+ (String)item[2]);
+        }
 
         sc.close();
     }
