fa35e10a8523264c550df0bab4e104291dfb1dd1
stops <stopsilver212@gmail.com>
2021-10-27 20:59:20 +0900

    N과 M(BackTracking)-미완

--- AlgorithmStudy/src/part3/week23/jje/BackTracking_15649.java
+++ AlgorithmStudy/src/part3/week23/jje/BackTracking_15649.java
@@ -1,11 +1,38 @@
 package part3.week23.jje;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BackTracking_15649 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+    static Scanner sc = new Scanner(System.in);
+    static int n = sc.nextInt();
+    static int m = sc.nextInt();
+
+    public static void main(String[] args) {
+
+        //for (int i = 1; i <= n; i++) {
+            checkBefore(0);
+        //}
         sc.close();
+    }
+    static int temp = 0;
+    static ArrayList<Integer> resultArray = new ArrayList<>();
+    static void checkBefore(int num){
+        if(resultArray.size() == m){
+            for (int i = 0; i < resultArray.size(); i++) {
+                System.out.println(resultArray.get(i));
+            }
+            return;
+        }
+
+        for (int i = 0; i < m; i++) {
+            if(resultArray.get(i) == num){
+                checkBefore(num+1);
+            }else {
+                resultArray.add(num);
+            }
+        }
+
     }
 }
