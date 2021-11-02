329e83995906833b2cd95c7455a495a3d10cedee
stops <stopsilver212@gmail.com>
2021-11-02 21:56:42 +0900

    단어 정렬

--- AlgorithmStudy/src/part3/week24/jje/Sort_1181.java
+++ AlgorithmStudy/src/part3/week24/jje/Sort_1181.java
@@ -1,12 +1,35 @@
 package part3.week24.jje;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Sort_1181 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] dupleArr = new String[n];
+        for (int i = 0; i < n; i++) {
+            dupleArr[i] = sc.next();
+        }
+        HashSet<String> set = new HashSet<>(Arrays.asList(dupleArr));
+        String[] resultArr = set.toArray(new String[0]);
 
+        Arrays.sort(resultArr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        for (int i = 0; i < resultArr.length; i++) {
+            System.out.println(resultArr[i]);
+        }
         sc.close();
-
     }
 }
