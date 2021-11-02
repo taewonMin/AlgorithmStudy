c8d04b2a4da75fd6d8dfbb3ce28b4d27db25e83f
Ia7756 <gramian7756@gmail.com>
2021-11-02 20:32:51 +0900

    DP

---
+++ AlgorithmStudy/src/part3/week23/psw/BackTracking_9663_3.java
@@ -0,0 +1,7 @@
+package part3.week23.psw;
+
+public class BackTracking_9663_3 {
+    public static void main(String[] args) {
+        System.out.println(new int[]{0,1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184}[new java.util.Scanner(System.in).nextInt()]);
+    }
+}
--- AlgorithmStudy/src/part3/week23/psw/DP_2579.java
+++ AlgorithmStudy/src/part3/week23/psw/DP_2579.java
@@ -3,20 +3,39 @@ package part3.week23.psw;
 import java.util.Scanner;
 
 public class DP_2579 {
+    static Integer dp[];
+    static int arr[];
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = sc.nextInt();
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        dp = new Integer[N + 1];
+        arr = new int[N + 1];
+
+        for(int i = 1; i <= N; i++) {
+            arr[i] = in.nextInt();
         }
 
-        int[] answer = new int[n];
-        boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            answer[i+1] = answer[i] + numbers[i+1];
-            answer[i+2] = answer[i] + numbers[i+2];
+        dp[0] = arr[0];	// 디폴트값이 null이므로 0으로 초기화 해주어야한다.
+        dp[1] = arr[1];
+
+        if(N >= 2) {
+            dp[2] = arr[1] + arr[2];
         }
-        sc.close();
+
+        System.out.println(find(N));
+
     }
+
+    static int find(int N) {
+        // 아직 탐색하지 않는 N번째 계단일 경우
+        if(dp[N] == null) {
+            dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1]) + arr[N];
+        }
+
+        return dp[N];
+    }
+
 }
