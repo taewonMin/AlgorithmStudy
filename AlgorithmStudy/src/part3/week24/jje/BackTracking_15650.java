afbac064e224ef4f581c43767f9b59f1e0aaeef0
stops <stopsilver212@gmail.com>
2021-11-02 21:55:50 +0900

    N과 M(BackTracking)2

--- AlgorithmStudy/src/part3/week24/jje/BackTracking_15650.java
+++ AlgorithmStudy/src/part3/week24/jje/BackTracking_15650.java
@@ -3,9 +3,34 @@ package part3.week24.jje;
 import java.util.Scanner;
 
 public class BackTracking_15650 {
+
+    static int n = 0;
+    static int m = 0;
+    static int[] arr = null;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[m];
+        dfs(1, 0);
         sc.close();
+    }
+
+    static void dfs(int index, int depth){
+        if(depth == m){
+            for(int result : arr){
+                System.out.println(result);
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = index; i <= n ; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
     }
 }
