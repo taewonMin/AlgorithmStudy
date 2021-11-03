f9ae0fb56d9dc3a204b35dc6d4c7e1e3641fddaa
Jieun Jeong <stopsilver@kaoni.com>
2021-11-03 16:56:35 +0900

    단어정렬(퀵정렬)

--- AlgorithmStudy/src/part3/week24/jje/Sort_1181.java
+++ AlgorithmStudy/src/part3/week24/jje/Sort_1181.java
@@ -15,7 +15,8 @@ public class Sort_1181 {
         }
         HashSet<String> set = new HashSet<>(Arrays.asList(dupleArr));
         String[] resultArr = set.toArray(new String[0]);
-
+        sort(resultArr, 0, resultArr.length - 1);
+        /*
         Arrays.sort(resultArr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -26,10 +27,37 @@ public class Sort_1181 {
                 }
             }
         });
-
+        */
         for (int i = 0; i < resultArr.length; i++) {
             System.out.println(resultArr[i]);
         }
         sc.close();
+    }
+    static void sort(String[] arr, int low, int high){
+        if (low >= high) return;
+
+        int mid = partition(arr, low, high); //재귀 호출할 때 우측 리스트의 시작 인덱스
+        sort(arr, low, mid - 1);
+        sort(arr, mid, high);
+    }
+
+    static int partition(String[] arr, int low, int high){
+        int pivot = arr[(low + high) / 2].length();
+        while (low <= high) {
+            while (arr[low].length() < pivot) low ++;
+            while (arr[high].length() > pivot) high--;
+            if (low <= high) {
+               swap(arr, low, high);
+               low++;
+               high--;
+            }
+        }
+        return low;
+    }
+
+    static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
