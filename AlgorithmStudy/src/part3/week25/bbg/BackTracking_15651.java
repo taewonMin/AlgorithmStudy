02652b9ce9a57b0d8162b2634f7ee3879e2ecd0b
gilbeee <7mini96@naver.com>
2021-11-09 01:36:07 +0900

    15651

--- AlgorithmStudy/src/part3/week25/bbg/BackTracking_15651.java
+++ AlgorithmStudy/src/part3/week25/bbg/BackTracking_15651.java
@@ -1,11 +1,39 @@
 package part3.week25.bbg;
 
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*N&M  113148KB 496ms*/
 public class BackTracking_15651 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        sc.close();
+	static int[] arr = null;
+	static StringBuffer sb = new StringBuffer();
+	
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        br.close();
+
+        arr = new int[M];
+        bfs(N,M,0);
+        System.out.println(sb);
+    }
+    
+    static void bfs(int N, int M, int depth) {
+    	if(depth == M) { // 깊이와 숫자 갯수가 같으면 출력
+    		for(int item : arr){
+    			sb.append(item).append(" ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for(int i = 0 ; i < N ; i++) {
+    		arr[depth] = i+1;
+    		bfs(N,M,depth+1);
+    	}
     }
 }
