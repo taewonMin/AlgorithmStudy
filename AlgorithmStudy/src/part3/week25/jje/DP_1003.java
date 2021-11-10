ba13f9333ba18e26b3da976293f84e007815aefb
stops <stopsilver212@gmail.com>
2021-11-10 21:01:48 +0900

    피보나치 함수

--- AlgorithmStudy/src/part3/week25/jje/DP_1003.java
+++ AlgorithmStudy/src/part3/week25/jje/DP_1003.java
@@ -3,9 +3,34 @@ package part3.week25.jje;
 import java.util.Scanner;
 
 public class DP_1003 {
+
+    static Integer[][] dp = new Integer[41][2];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        sc.close();
+        dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
+        dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
+        dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
+        dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int N = sc.nextInt();
+            fibonacci(N);
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+    }
+
+    static Integer[] fibonacci(int N) {
+        // N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
+        if(dp[N][0] == null || dp[N][1] == null) {
+            // 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        // N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
+        return dp[N];
     }
 }
