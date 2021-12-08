d5c6db7cd8ca4af9eecfa2d879798faa90e7ec1a
Jieun Jeong <stopsilver@kaoni.com>
2021-12-08 17:59:27 +0900

    부녀회장이 될 테야

--- AlgorithmStudy/src/part3/week27/jje/Math_2775.java
+++ AlgorithmStudy/src/part3/week27/jje/Math_2775.java
@@ -1,7 +1,45 @@
 package part3.week27.jje;
 
-public class Math_2775 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Math_2775 {
+
+    static int[][] arr = new int[15][15];
+
+    public static void main(String[] args) {
+        /*
+        1 2 3 4
+        1 3 6 10
+        1 4 10 20
+        1 5 15 35
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int k = 0;
+        int n = 0;
+
+        for (int i = 0; i < 15; i++) {
+            arr[0][i] = i + 1;
+            arr[i][0] = 1;
+        }
+
+        for (int i = 0; i < t; i++) {
+            k = sc.nextInt();
+            n = sc.nextInt()-1;
+            if (k > 0 && n > 0) {
+                apartment(k, n);
+            }
+            System.out.println(arr[k][n]);
+        }
+    }
+
+    static void apartment(int k, int n){
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = arr[i - 1][j]+arr[i][j-1];
+            }
+        }
     }
 }
