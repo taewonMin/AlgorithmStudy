fd1fa55e022dd2839ebefb788b0574ad15890ae1
stops <stopsilver212@gmail.com>
2021-12-08 21:00:31 +0900

    설탕 배달

--- AlgorithmStudy/src/part3/week27/jje/Math_2839.java
+++ AlgorithmStudy/src/part3/week27/jje/Math_2839.java
@@ -1,7 +1,59 @@
 package part3.week27.jje;
 
+import java.util.Scanner;
+
 public class Math_2839 {
     public static void main(String[] args) {
+        //3 5
+
+        Scanner sc = new Scanner(System.in);
+                /*
+        int n = sc.nextInt();
+        int result = 0;
+        while(n > 2) {
+            if(n % 5 == 0) {
+                result += n / 5;
+                break;
+            } else {
+                n -= 3;
+                result++;
+            }
+        }
+        if(n < 3) {
+            System.out.println(-1);
+        } else{
+            System.out.println(result);
+        }
 
     }
+    */
+
+        int[] dp = new int[5001];
+        int n = sc.nextInt();
+
+        dp[3] = 1; dp[4] = -1; dp[5] = 1; dp[6] = 2; dp[7] = -1;
+
+        for (int i = 8; i <= n; i++) {
+                int[] arr = {dp[i - 5], dp[i - 3]};
+                dp[i] = min(arr) + 1;
+        }
+
+        System.out.println(dp[n]);
+        sc.close();
+    }
+
+    static int min(int[] arr) {
+        int min = arr[0];
+        for (int num : arr) {
+            if (min != -1 && num != -1){
+                if (num < min) {
+                    min = num;
+                }
+            }else {
+                min = Math.max(min, num);
+            }
+        }
+        return min;
+    }
+
 }
