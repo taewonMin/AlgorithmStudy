071f46e17bd307f5c22a12829cd6c79e491978f3
smarto <mtw9411@naver.com>
2021-12-08 20:48:17 +0900

    설탕 배달

--- AlgorithmStudy/src/part3/week27/mtw/Math_2839.java
+++ AlgorithmStudy/src/part3/week27/mtw/Math_2839.java
@@ -1,7 +1,57 @@
 package part3.week27.mtw;
 
-public class Math_2839 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class Math_2839 {
+
+    public static int[] dp = new int[5001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dp[3] = 1;
+        dp[4] = -1;
+        dp[5] = 1;
+
+        int N = Integer.parseInt(br.readLine());
+        
+        bw.write(recursion(N)+"");
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+        // 방법 2
+        // int cnt = 0;
+        // while(true) {
+        //     if(N%5 == 0) {
+        //         System.out.println(N/5 + cnt);
+        //         break;
+        //     }
+        //     if(N < 3) {
+        //         System.out.println(-1);
+        //         break;
+        //     }
+        //     N -= 3;
+        //     cnt++;
+        // }
+    }
+
+    static int recursion(int n){
+        if(n < 3) return -1;
+        if(dp[n] != 0) return dp[n];
+        int temp1 = recursion(n-5)+1;
+        int temp2 = recursion(n-3)+1;
+        if(temp1 == 0 & temp2 == 0){
+            return dp[n] = -1;
+        }
+        temp1 = temp1==0 ? Integer.MAX_VALUE : temp1;
+        temp2 = temp2==0 ? Integer.MAX_VALUE : temp2;
+        
+        return dp[n] = Math.min(temp1, temp2);
     }
 }
