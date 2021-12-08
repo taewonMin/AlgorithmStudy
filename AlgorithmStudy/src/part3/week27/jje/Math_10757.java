c0ea96595cd4aa93eb11971e247d47eec9b41fa0
Jieun Jeong <stopsilver@kaoni.com>
2021-12-08 17:59:57 +0900

    큰 수

--- AlgorithmStudy/src/part3/week27/jje/Math_10757.java
+++ AlgorithmStudy/src/part3/week27/jje/Math_10757.java
@@ -1,7 +1,11 @@
 package part3.week27.jje;
 
+import java.util.Scanner;
+
 public class Math_10757 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
     }
 }
