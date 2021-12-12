d1cb26cb1087e59cc3a000af10049cc4b685d9c0
gilbeee <7mini96@naver.com>
2021-12-13 01:09:46 +0900

    27

--- AlgorithmStudy/src/part3/week27/bbg/Math_10757.java
+++ AlgorithmStudy/src/part3/week27/bbg/Math_10757.java
@@ -1,7 +1,17 @@
 package part3.week27.bbg;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Math_10757 {
     public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
 
+		BigDecimal A = new BigDecimal(sc.next());
+		BigDecimal B = new BigDecimal(sc.next());
+		
+		sc.close();
+		
+		System.out.println(A.add(B));
     }
 }
--- AlgorithmStudy/src/part3/week27/bbg/Math_2839.java
+++ AlgorithmStudy/src/part3/week27/bbg/Math_2839.java
@@ -1,7 +1,37 @@
 package part3.week27.bbg;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Math_2839 {
     public static void main(String[] args) {
-
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	sc.close();
+    	
+    	List<Integer> list = new ArrayList<Integer>();
+    	
+    	int cnt = 0;
+    	for(int i = N/5 ; i >= 0 ; i--) {
+    		for(int j = 0 ; j < N/3 ; j++) {
+    			if(5 * i + 3 * j == N) {
+    				list.add(i+j);
+    			}
+    		}
+    	}
+    	if(list.size() == 0)
+	    	for(int j = 0 ; j <= N/3 ; j++) {
+				if(3 * j == N) 
+					list.add(j);
+			}
+    	
+    	list.sort(Comparator.reverseOrder());
+    	
+    	System.out.println(list.size() == 0 ? -1 : list.get(0));
+    	
     }
 }
