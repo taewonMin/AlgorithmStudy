4b9756dc3a434c0ab59df6c4c0154eccfde318a3
Jieun Jeong <stopsilver@kaoni.com>
2021-12-13 12:54:29 +0900

    파도반 수열

--- AlgorithmStudy/src/part3/week28/jje/DP_9461.java
+++ AlgorithmStudy/src/part3/week28/jje/DP_9461.java
@@ -3,9 +3,29 @@ package part3.week28.jje;
 import java.util.Scanner;
 
 public class DP_9461 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static long[] arr = new long[101];
 
+    public static void main(String[] args) {
+
+        arr[1] = 1; arr[2] = 1; arr[3] = 1;
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n = 0;
+        long result = 0;
+        for (int i = 0; i < t; i++) {
+            n = sc.nextInt();
+            if(arr[n] == 0) {
+                makeTriangle(n);
+            }
+            System.out.println(arr[n]);
+        }
         sc.close();
+    }
+
+    static void makeTriangle(int n){
+        for (int i = 4; i <= n; i++) {
+            if(arr[i] == 0 )  arr[i] = arr[i - 2] + arr[i - 3];
+        }
     }
 }
