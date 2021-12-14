00673d3d7cf906f574d105b73d98c75ad9030548
smarto <mtw9411@naver.com>
2021-12-15 00:20:00 +0900

    01타일

--- AlgorithmStudy/src/part3/week28/mtw/DP_1904.java
+++ AlgorithmStudy/src/part3/week28/mtw/DP_1904.java
@@ -2,13 +2,48 @@ package part3.week28.mtw;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class DP_1904 {
-    public static void main(String[] args) {
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N+2];
+        Arrays.fill(dp, -1);
+        dp[1] = 1;
+        dp[2] = 2;
+
+        bw.write(recursion(N) + "");
+        // bw.write(iterator(N) + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    // 336ms 55844KB
+    public static int recursion(int N){
+        if(dp[N] != -1){
+            return dp[N];
+        }
+        return dp[N] = (recursion(N-1) + recursion(N-2))%15746;
+    }
+
+    // 148ms 18172KB
+    public static int iterator(int N){
+        if(N < 3){
+            return N;
+        }else{
+            for(int i=3; i<=N; i++){
+                dp[i] = (dp[i-1] + dp[i-2])%15746;
+            }
+            return dp[N];
+        }
     }
 }
