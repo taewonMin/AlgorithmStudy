7878b69836fb8dcdb7da025d3332ae45837eef90
smarto <mtw9411@naver.com>
2021-12-15 00:34:06 +0900

    파도반 수열

--- AlgorithmStudy/src/part3/week28/mtw/DP_9461.java
+++ AlgorithmStudy/src/part3/week28/mtw/DP_9461.java
@@ -2,13 +2,39 @@ package part3.week28.mtw;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class DP_9461 {
-    public static void main(String[] args) {
+
+    public static long[] dp = new long[101];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++){
+            int N = Integer.parseInt(br.readLine());
+            bw.write(triangle(N)+"");
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static long triangle(int N){
+        if(dp[N] == 0){
+            dp[N] = triangle(N-1) + triangle(N-5);
+        }
+        return dp[N];
     }
 }
