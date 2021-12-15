c4264b0f296adc8555859b1e18410aa631206694
stops <stopsilver212@gmail.com>
2021-12-15 21:16:31 +0900

    01타일

--- AlgorithmStudy/src/part3/week28/jje/DP_1904.java
+++ AlgorithmStudy/src/part3/week28/jje/DP_1904.java
@@ -4,8 +4,21 @@ import java.util.Scanner;
 
 public class DP_1904 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        int[] arr = new int[10000001];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //11 2
+        //12 21 111
+        //121 211 112 1111 22 4 -> 5
+        //1112 1121 1211 2111 122 221 11111  212 5 -> 8
+        //121
+        arr[2] = 2; arr[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+
+        System.out.println(arr[n] % 15746);
         sc.close();
     }
 }
