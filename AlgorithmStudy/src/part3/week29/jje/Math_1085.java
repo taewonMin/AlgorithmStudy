c3f43f5806bee97df65679b921f66c21ebffdc20
stops <stopsilver212@gmail.com>
2021-12-21 21:47:42 +0900

    직사각형에서 탈출

--- AlgorithmStudy/src/part3/week29/jje/Math_1085.java
+++ AlgorithmStudy/src/part3/week29/jje/Math_1085.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Math_1085 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
 
+        System.out.println(Math.min(Math.min(w - x, x), Math.min(h - y, y)));
         sc.close();
     }
 }
