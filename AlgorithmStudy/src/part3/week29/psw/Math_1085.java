911815d9e46ebb9d3e3309b4f2311569ecf6ea93
Ia7756 <gramian7756@gmail.com>
2021-12-21 10:01:39 +0900

    탈출

--- AlgorithmStudy/src/part3/week29/psw/Math_1085.java
+++ AlgorithmStudy/src/part3/week29/psw/Math_1085.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Math_1085 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int x=sc.nextInt(),y=sc.nextInt(),w=sc.nextInt(),h=sc.nextInt();
         sc.close();
+        System.out.println(Math.min(Math.min(w-x,x),Math.min(h-y,y)));
     }
 }
