c3b3147d79efe7e8ce78b08068bfca57a8ececef
smarto <mtw9411@naver.com>
2021-12-23 06:52:43 +0900

    네 번째 점

--- AlgorithmStudy/src/part3/week29/mtw/Math_3009.java
+++ AlgorithmStudy/src/part3/week29/mtw/Math_3009.java
@@ -1,12 +1,31 @@
 package part3.week29.mtw;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Math_3009 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<String> xList = new ArrayList<>();
+        List<String> yList = new ArrayList<>();
+        for(int i=0; i<3; i++){
+            String[] input = br.readLine().split(" ");
+            if(xList.contains(input[0])){
+                xList.remove(input[0]);
+            }else{
+                xList.add(input[0]);
+            }
+            if(yList.contains(input[1])){
+                yList.remove(input[1]);
+            }else{
+                yList.add(input[1]);
+            }
+        }
+
+        bw.write(xList.get(0) + " " + yList.get(0));
 
         br.close();
         bw.flush();
