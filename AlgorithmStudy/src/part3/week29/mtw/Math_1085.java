543b943f8c5cf7a5dcf394acf151eafbe8129dd1
smarto <mtw9411@naver.com>
2021-12-23 06:52:35 +0900

    직사각형에서 탈출

--- AlgorithmStudy/src/part3/week29/mtw/Math_1085.java
+++ AlgorithmStudy/src/part3/week29/mtw/Math_1085.java
@@ -1,13 +1,18 @@
 package part3.week29.mtw;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Math_1085 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String[] input = br.readLine().split(" ");
+        int x = Integer.parseInt(input[0]);
+        int y = Integer.parseInt(input[1]);
+        int w = Integer.parseInt(input[2]);
+        int h = Integer.parseInt(input[3]);
+        bw.write(Math.min(Math.min(x,y), Math.min(w-x, h-y))+"");
 
         br.close();
         bw.flush();
