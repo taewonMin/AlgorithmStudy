89c3ca19f3e2385372ba96b81ae2c3daef22a7af
Ia7756 <gramian7756@gmail.com>
2021-12-24 10:17:21 +0900

    직각삼각형

--- AlgorithmStudy/src/part3/week30/psw/Math_4153.java
+++ AlgorithmStudy/src/part3/week30/psw/Math_4153.java
@@ -1,11 +1,24 @@
 package part3.week30.psw;
 
 import java.util.Scanner;
-
+//	12872	116
 public class Math_4153 {
     public static void main(String[] args) {
+        String right = "right";
+        String wrong = "wrong";
+
         Scanner sc = new Scanner(System.in);
 
+        while (true){
+            int a = sc.nextInt(),b = sc.nextInt(),c = sc.nextInt();
+            if (a==0&&b==0&&c==0) break;
+            System.out.println(isTri(a,b,c) ? right : wrong);
+        }
+
         sc.close();
+    }
+
+    public static boolean isTri(int a, int b, int c){
+        return a*a+b*b == c*c || a*a+c*c == b*b || b*b+c*c ==a*a;
     }
 }
