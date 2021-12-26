dcf6016be830bb99730f51f1ff609dbf62259f63
smarto <mtw9411@naver.com>
2021-12-26 20:37:23 +0900

    직각삼각형

--- AlgorithmStudy/src/part3/week30/mtw/Math_4153.java
+++ AlgorithmStudy/src/part3/week30/mtw/Math_4153.java
@@ -2,15 +2,34 @@ package part3.week30.mtw;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Math_4153 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        while(true){
+            String[] input = br.readLine().split(" ");
+            int[] array = Arrays.stream(input).mapToInt(Integer::parseInt).sorted().toArray();
 
+            if(array[0]==0 && array[1]==0 && array[2]==0){
+                break;
+            }
+
+            if(array[0]*array[0] + array[1]*array[1] == array[2]*array[2]){
+                bw.write("right");
+            }else{
+                bw.write("wrong");
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
