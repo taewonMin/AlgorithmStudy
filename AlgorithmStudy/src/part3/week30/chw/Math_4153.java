db2d85bed1afdf4e3df57dac38e942991f37e133
hyeonwoo <tifhs43@naver.com>
2021-12-27 09:37:29 +0900

    직각삼각형 커밋

--- AlgorithmStudy/src/part3/week30/chw/Math_4153.java
+++ AlgorithmStudy/src/part3/week30/chw/Math_4153.java
@@ -1,11 +1,26 @@
 package part3.week30.chw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Math_4153 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+        	int[] arr = new int[3];
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int idx = 0;
+        	while(st.hasMoreTokens()) {
+        		arr[idx] = Integer.parseInt(st.nextToken());
+        		idx++;
+        	}
+        	Arrays.sort(arr);
+        	if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+        	System.out.println(arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2] ? "right" : "wrong");
+        }
+        br.close();
     }
 }
