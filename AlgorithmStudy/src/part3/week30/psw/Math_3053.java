6c9fb4c7abfcabde3918312bee135eae8a1bc9f4
Ia7756 <gramian7756@gmail.com>
2021-12-27 14:53:43 +0900

    택시

--- AlgorithmStudy/src/part3/week30/psw/Math_3053.java
+++ AlgorithmStudy/src/part3/week30/psw/Math_3053.java
@@ -1,11 +1,15 @@
 package part3.week30.psw;
 
 import java.util.Scanner;
-
+// 13040	112
 public class Math_3053 {
+    static double PI = Math.PI;
+    static String formatter = "%.5f";
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+        int r = new Scanner(System.in).nextInt();
+        System.out.print(round(PI*(r*r))+"\n"+round(2.0*(r*r)));
+    }
+    public static String round(double num){
+        return String.format(formatter,num);
     }
 }
