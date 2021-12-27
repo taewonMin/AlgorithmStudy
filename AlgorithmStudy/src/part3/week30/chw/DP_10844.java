03a3c7f804c833ff23363f939d5cde319a5ce7bf
Ia7756 <gramian7756@gmail.com>
2021-12-27 20:20:41 +0900

    30주 2차

---
+++ AlgorithmStudy/src/part3/week30/chw/DP_10844.java
@@ -0,0 +1,11 @@
+package part3.week30.chw;
+
+import java.util.Scanner;
+
+public class DP_10844 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
---
+++ AlgorithmStudy/src/part3/week30/mtw/DP_10844.java
@@ -0,0 +1,14 @@
+package part3.week30.mtw;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class DP_10844 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
