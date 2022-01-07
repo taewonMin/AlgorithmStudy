3a190a45c2463775905656009e904beb0fbb249a
Ia7756 <gramian7756@gmail.com>
2022-01-07 11:21:29 +0900

    포도주

--- AlgorithmStudy/src/part4/week31/psw/DP_2156.java
+++ AlgorithmStudy/src/part4/week31/psw/DP_2156.java
@@ -1,13 +1,20 @@
 package part4.week31.psw;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.util.Arrays;
+import java.util.Scanner;
+//32640	448
 public class DP_2156 {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[][] dp = new int[t+1][3];
+        for (int i = 1; i < t+1; i++) {
+            int n = sc.nextInt();
+            dp[i][0] = Arrays.stream(dp[i-1]).max().getAsInt();
+            dp[i][1] = dp[i-1][0]+n;
+            dp[i][2] = dp[i-1][1]+n;
+        }
+        System.out.println(Arrays.stream(dp[t]).max().getAsInt());
+        sc.close();
     }
 }
