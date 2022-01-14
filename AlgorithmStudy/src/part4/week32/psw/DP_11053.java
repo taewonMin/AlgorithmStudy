696ad8e4a27eb29ff679d74a60a9094761820020
Ia7756 <gramian7756@gmail.com>
2022-01-14 13:24:25 +0900

    백준 / DP / LIS 1

--- AlgorithmStudy/src/part4/week32/psw/DP_11053.java
+++ AlgorithmStudy/src/part4/week32/psw/DP_11053.java
@@ -1,13 +1,30 @@
 package part4.week32.psw;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class DP_11053 {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[] numbers = new int[length];
+        int[] dp = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < length; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] > numbers[j] && max < dp[j]) {
+                    max = dp[j];
+                }
+            }
+            dp[i] = max +1;
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+        sc.close();
     }
 }
