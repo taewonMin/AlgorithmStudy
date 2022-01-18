b54117a368a1a9140c07046934f7e6ac0997aca8
user <user@DESKTOP-4R5DN4R>
2022-01-19 01:01:25 +0900

    32주차 커밋

--- AlgorithmStudy/src/part4/week32/chw/DP_11054.java
+++ AlgorithmStudy/src/part4/week32/chw/DP_11054.java
@@ -2,12 +2,62 @@ package part4.week32.chw;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class DP_11054 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int max = 0;
+        int[] arr = new int[N];
+        int[] lis_dp = new int[N];
+        int[] lds_dp = new int[N];
+        lis_dp[0] = 1;
+        lds_dp[0] = 1;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int idx = 0;
+        while(st.hasMoreTokens()) {
+        	arr[idx] = Integer.parseInt(st.nextToken());
+        	idx++;
+        }
+        
+        // LIS
+        for(int i = 0; i < N; i++) {
+        	lis_dp[i] = 1;
+        	for(int j = 0; j < i; j++) {
+        		if(arr[i] > arr[j] && lis_dp[i] < lis_dp[j] + 1) {
+        			lis_dp[i] = lis_dp[j] + 1;
+        		}
+        	}
+        }
+        
+        // LDS
+        for(int i = N - 1; i >= 0; i--) {
+        	lds_dp[i] = 1;
+        	for(int j = N - 1; j > i; j--) {
+        		if(arr[i] > arr[j] && lds_dp[i] < lds_dp[j] + 1) {
+        			lds_dp[i] = lds_dp[j] + 1;
+        		}
+        	}
+        }
+        
+        for(int i = 0; i < N; i++) {
+        	max = Math.max(lds_dp[i] + lis_dp[i], max);
+        }
+        
+        bw.write(String.valueOf(max-1));
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+/*
+[8, 2, 9, 1, 4, 6, 7, 10]
+[3, 2, 2, 1, 1, 1, 1, 1]
+
+*/
--- AlgorithmStudy/src/part4/week32/chw/DP_2565.java
+++ AlgorithmStudy/src/part4/week32/chw/DP_2565.java
@@ -2,12 +2,69 @@ package part4.week32.chw;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class DP_2565 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        int[] dp = new int[N];
+        int result = 0;
+        dp[0] = 1;
+        for(int i = 0; i < N; i++) {
+        	String[] numbers = br.readLine().split(" ");
+        	arr[i][0] = Integer.parseInt(numbers[0]);
+        	arr[i][1] = Integer.parseInt(numbers[1]);
+        }
+        
+        // 정렬
+        Arrays.sort(arr, Comparator.comparingInt(o1 -> o1[0]));
+
+        for(int i = 0; i < N; i++) {
+        	dp[i] = 1;
+        	for(int j = 0; j < i; j++) {
+        		if(arr[i][1] > arr[j][1] && dp[i] < dp[j] + 1) {
+        			dp[i] = dp[j] + 1;
+        		}
+        	}
+        	result = Math.max(result, dp[i]);
+        }
+        bw.write(String.valueOf(N-result));
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+/*
+8
+
+1 8
+3 9
+2 2 or 4 1
+
+6 4
+7 6
+9 7
+10 10
+
+
+1 8
+2 2
+3 9
+4 1
+6 4
+7 6
+9 7
+10 10
+
+8 2 9 1 4 6 7 10
+1 1 2 1 2 3 4 5
+3 2 2 1 1 1 1 1  max=3
+
+*/
