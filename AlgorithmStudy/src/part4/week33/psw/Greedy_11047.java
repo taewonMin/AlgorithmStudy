7c2382f6e7a9a73b51b884f6ad85de9fa04b4160
Ia7756 <gramian7756@gmail.com>
2022-01-24 10:14:46 +0900

    백준 / 그리디 / 동전 0

--- AlgorithmStudy/src/part4/week33/psw/Greedy_11047.java
+++ AlgorithmStudy/src/part4/week33/psw/Greedy_11047.java
@@ -1,11 +1,31 @@
 package part4.week33.psw;
 
 import java.util.Scanner;
-
+//12896	112
 public class Greedy_11047 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+        int[] numbers = new int[n];
+        int maxIdx = 0;
 
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            numbers[i] = number;
+            if (number <= k) maxIdx = Math.max(maxIdx,i);
+        }
+
+        int answer = 0;
+        while (true){
+            if(numbers[maxIdx] <= k){
+                answer += k/numbers[maxIdx];
+                k = k%numbers[maxIdx];
+            }
+            maxIdx--;
+            if (k == 0) break;
+        }
+
+        System.out.println(answer);
         sc.close();
     }
 }
