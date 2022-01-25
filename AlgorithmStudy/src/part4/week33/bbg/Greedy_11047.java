c75fe6ad568d468a8976c77b2f35e3ad0286194d
gilbeee <7mini96@naver.com>
2022-01-26 00:30:24 +0900

    동전 0

--- AlgorithmStudy/src/part4/week33/bbg/Greedy_11047.java
+++ AlgorithmStudy/src/part4/week33/bbg/Greedy_11047.java
@@ -1,11 +1,37 @@
 package part4.week33.bbg;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Greedy_11047 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
-    }
+public class Greedy_11047 {// 동전 0 java 8 11592KB	84ms 1088B
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] coins = new int[N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int quantity = 0;
+		int nam = K;
+		for(int i = N-1 ; i >= 0 && nam > 0 ; i--) {
+			if(coins[i] > K) continue;
+			quantity += nam / coins[i];
+			nam = K % coins[i]; // K가 아니라 nam에 대한 나머지를 구해야 한다.
+		}
+		
+		bw.write(String.valueOf(quantity));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
 }
