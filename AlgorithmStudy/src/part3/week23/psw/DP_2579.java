8bdea27073673b3fbafe5f682f225824f350d862
Ia7756 <gramian7756@gmail.com>
2022-01-26 14:11:16 +0900

    백준 / DP / 계단오르기

--- AlgorithmStudy/src/part3/week23/psw/DP_2579.java
+++ AlgorithmStudy/src/part3/week23/psw/DP_2579.java
@@ -1,41 +1,21 @@
 package part3.week23.psw;
 
 import java.util.Scanner;
-
+//13364	128
 public class DP_2579 {
-    static Integer dp[];
-    static int arr[];
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        int N = in.nextInt();
-
-        dp = new Integer[N + 1];
-        arr = new int[N + 1];
-
-        for(int i = 1; i <= N; i++) {
-            arr[i] = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] steps = new int[N+1], dp = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            steps[i] = sc.nextInt();
+            if(i==1) dp[i]=steps[i];
+            if(i==2) dp[i]=steps[i]+dp[i-1];
         }
 
-        dp[0] = arr[0];	// 디폴트값이 null이므로 0으로 초기화 해주어야한다.
-        dp[1] = arr[1];
-
-        if(N >= 2) {
-            dp[2] = arr[1] + arr[2];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = steps[i] + Math.max(dp[i-2],dp[i-3]+steps[i-1]);
         }
-
-        System.out.println(find(N));
-
+        System.out.println(dp[N]);
     }
-
-    static int find(int N) {
-        // 아직 탐색하지 않는 N번째 계단일 경우
-        if(dp[N] == null) {
-            dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1]) + arr[N];
-        }
-
-        return dp[N];
-    }
-
 }
