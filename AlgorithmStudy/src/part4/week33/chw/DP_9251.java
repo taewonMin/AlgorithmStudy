5072f77a8afd761f824cefa696bd7435769604e2
hyeonwoo <tifhs43@naver.com>
2022-01-26 21:00:25 +0900

    33주차 커밋

--- AlgorithmStudy/src/part4/week33/chw/DP_9251.java
+++ AlgorithmStudy/src/part4/week33/chw/DP_9251.java
@@ -3,8 +3,25 @@ package part4.week33.chw;
 import java.io.*;
 
 public class DP_9251 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int[][] arr = new int[str1.length()+1][str2.length()+1];
+        
+        for(int i = 1; i <= str1.length(); i++) {
+        	for(int j = 1; j <= str2.length(); j++) {
+        		if(str1.charAt(i-1) == str2.charAt(j-1)) {
+        			arr[i][j] = arr[i-1][j-1] + 1; 
+        		}else {
+        			arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+        		}
+        	}
+        }
+        bw.write(String.valueOf(arr[str1.length()][str2.length()]));
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
--- AlgorithmStudy/src/part4/week33/chw/Greedy_11047.java
+++ AlgorithmStudy/src/part4/week33/chw/Greedy_11047.java
@@ -3,8 +3,50 @@ package part4.week33.chw;
 import java.io.*;
 
 public class Greedy_11047 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] arr = br.readLine().split(" ");
+        int N = Integer.parseInt(arr[0]);
+        int K = Integer.parseInt(arr[1]);
+        int[] coin = new int[N];
+        int result = 0;
+        int idx = 0;
+        for(int i = 0; i < N; i++) {
+        	coin[i] = Integer.parseInt(br.readLine());
+        	if(coin[i] > K) {
+        		idx = i - 1;
+        		coin[i] = 0;
+        		break;
+        	}
+        }
+        for(int j = idx; j >= 0; j--) {
+        	if(coin[j] <= K) {
+				result += (K / coin[j]);
+				K = K % coin[j];
+			}
+        }
+//        for(int j = idx; j >= 0; j--) {
+//        	if(K == 0) break;
+//        	result += K / arr[j];
+//        	K -= (K / arr[j]) * arr[j];
+//        }
+        bw.write(String.valueOf(result));
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+/*
+10 4269
+1
+5
+10
+50
+100
+500
+1000
+5000
+10000
+50000
+*/
