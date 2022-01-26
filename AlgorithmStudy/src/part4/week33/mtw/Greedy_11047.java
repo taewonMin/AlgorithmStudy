a22d73d7895f713b0ca62187f482a9e778e4a972
smarto <mtw9411@naver.com>
2022-01-26 20:47:30 +0900

    동전 0

--- AlgorithmStudy/src/part4/week33/mtw/Greedy_11047.java
+++ AlgorithmStudy/src/part4/week33/mtw/Greedy_11047.java
@@ -3,8 +3,34 @@ package part4.week33.mtw;
 import java.io.*;
 
 public class Greedy_11047 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        
+        int[] coin = new int[N];
+        for(int i=0; i<N; i++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        int remain = K;
+        int coinIndex = coin.length-1;
+        while(remain > 0){
+            while(remain / coin[coinIndex] == 0){
+                coinIndex--;
+            }
+            count += remain / coin[coinIndex];
+            remain %= coin[coinIndex];
+        }
+
+        bw.write(count+"");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
