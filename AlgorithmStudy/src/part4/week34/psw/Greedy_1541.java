cd1b6adf3a81dc2d54dd665eaf4899822f9f400b
Ia7756 <gramian7756@gmail.com>
2022-01-27 18:46:31 +0900

    백준 / 탐욕법 / 잃어버린 괄호

--- AlgorithmStudy/src/part4/week34/psw/Greedy_1541.java
+++ AlgorithmStudy/src/part4/week34/psw/Greedy_1541.java
@@ -1,7 +1,20 @@
 package part4.week34.psw;
 
+import java.util.Scanner;
+//	12836	124
 public class Greedy_1541 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String[] calculations = sc.nextLine().split("-");
+        int answer = 0;
+        for (int i = 0; i < calculations.length; i++) {
+            String[] numbers = calculations[i].split("\\+");
+            int sum = 0;
+            for (int j = 0; j < numbers.length; j++) {
+                sum += Integer.parseInt(numbers[j]);
+            }
+            answer += i>0? -(sum) : sum;
+        }
+        System.out.println(answer);
     }
 }
