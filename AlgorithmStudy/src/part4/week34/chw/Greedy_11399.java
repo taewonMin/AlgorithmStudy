30c34ba76043838922289f4a9dae1effa0e0c60d
hyeonwoo <tifhs43@naver.com>
2022-02-03 17:47:10 +0900

    34주차 그리디 커밋

--- AlgorithmStudy/src/part4/week34/chw/Greedy_11399.java
+++ AlgorithmStudy/src/part4/week34/chw/Greedy_11399.java
@@ -1,7 +1,23 @@
 package part4.week34.chw;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Greedy_11399 {
     public static void main(String[] args) {
-
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	int[] arr = new int[N];
+    	int result = 0;
+    	for(int i = 0; i < N; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	Arrays.sort(arr);
+    	for(int minute : arr) {
+    		result += minute * N;
+    		N--;
+    	}
+    	System.out.println(result);
+    	sc.close();
     }
 }
--- AlgorithmStudy/src/part4/week34/chw/Greedy_1541.java
+++ AlgorithmStudy/src/part4/week34/chw/Greedy_1541.java
@@ -1,7 +1,25 @@
 package part4.week34.chw;
 
+import java.util.Scanner;
+
 public class Greedy_1541 {
     public static void main(String[] args) {
-
+    	Scanner sc = new Scanner(System.in);
+    	String[] inputArr = sc.next().split("-");
+    	int result = 0;
+    	for(int i = 0; i < inputArr.length; i++) {
+    		String[] arr = inputArr[i].split("\\+");
+    		int sum = 0;
+    		for(String str : arr) {
+    			sum += Integer.parseInt(str);
+    		}
+    		if(i == 0) {
+    			result = sum;
+    		}else {
+    			result -= sum;
+    		}
+    	}
+    	System.out.println(result);
+    	sc.close();
     }
 }
