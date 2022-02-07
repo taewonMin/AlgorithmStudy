414af90c8c2db581a8e52033765ec2dbac0da854
Jieun Jeong <stopsilver@kaoni.com>
2022-02-07 19:51:29 +0900

    Revert "파도반 수열"
    This reverts commit 2bca32c72f84b7deec2be4a4f13ea67458d2c227.

--- AlgorithmStudy/src/part4/week34/jje/Greedy_11399.java
+++ AlgorithmStudy/src/part4/week34/jje/Greedy_11399.java
@@ -1,27 +1,7 @@
 package part4.week34.jje;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Greedy_11399 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            result += arr[i] * (arr.length - i);
-        }
-
-        System.out.println(result);
-
-        sc.close();
     }
 }
