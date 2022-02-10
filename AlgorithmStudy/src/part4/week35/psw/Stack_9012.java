65e42985f70fd81a82ff4dbc009691f258929730
Ia7756 <gramian7756@gmail.com>
2022-02-10 15:34:04 +0900

    백준 / 스택 / 괄호

--- AlgorithmStudy/src/part4/week35/psw/Stack_9012.java
+++ AlgorithmStudy/src/part4/week35/psw/Stack_9012.java
@@ -1,7 +1,27 @@
 package part4.week35.psw;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+//	101660	1060
 public class Stack_9012 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), cnt = 0, idx = 0;
+        int[] numbers = Arrays.stream(new int[n]).map((a) -> sc.nextInt()).toArray();
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer sb = new StringBuffer();
+        while (cnt++ < n){
+            stack.push(cnt);
+            sb.append("+\n");
 
+            while (!stack.isEmpty() && stack.peek() == numbers[idx]){
+                stack.pop();
+                sb.append("-\n");
+                idx++;
+            }
+        }
+        System.out.println(idx==n ? sb.toString() : "NO");
     }
 }
