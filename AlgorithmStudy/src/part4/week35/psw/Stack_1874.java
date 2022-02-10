5776ca032eee21ab0ffff21d5951a73d3f43dd18
Ia7756 <gramian7756@gmail.com>
2022-02-10 15:33:53 +0900

    백준 / 스택 / 스택수열

--- AlgorithmStudy/src/part4/week35/psw/Stack_1874.java
+++ AlgorithmStudy/src/part4/week35/psw/Stack_1874.java
@@ -1,7 +1,30 @@
 package part4.week35.psw;
 
+import java.util.Scanner;
+import java.util.Stack;
+
+//	13456	136
 public class Stack_1874 {
     public static void main(String[] args) {
-
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            char[] data = sc.next().toCharArray();
+            Stack<Character> stack = new Stack<>();
+            String answer = "YES";
+            for (int j = data.length-1; j > -1; j--) {
+                if (data[j]==')') {
+                    stack.push(')');
+                } else {
+                    if (!stack.isEmpty()){
+                        stack.pop();
+                    }else {
+                        answer="NO";
+                    }
+                }
+            }
+            if (!stack.isEmpty()) answer = "NO";
+            System.out.println(answer);
+        }
     }
 }
