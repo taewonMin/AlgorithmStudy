3fdef129c64e7edd22cce8de9bb6a9ef09472259
smarto <mtw9411@naver.com>
2022-02-12 22:58:29 +0900

    괄호

--- AlgorithmStudy/src/part4/week35/mtw/Stack_9012.java
+++ AlgorithmStudy/src/part4/week35/mtw/Stack_9012.java
@@ -1,7 +1,42 @@
 package part4.week35.mtw;
 
-public class Stack_9012 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
+// 괄호
+public class Stack_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Stack<Character> stack = new Stack<Character>();
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+        	for(char brace : br.readLine().toCharArray()) {
+        		if(brace == '(') {
+        			stack.push(brace);
+        		}else {
+        			if(stack.isEmpty()) {
+        				stack.push(brace);
+        				break;
+        			}else if(stack.peek() == '(') {
+        				stack.pop();
+        			}else {
+        				stack.push(brace);
+        			}
+        		}
+        	}
+        	bw.write(stack.isEmpty() ? "YES":"NO");
+        	bw.newLine();
+        	stack.clear();
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
