7786327551986d2dfe8bbae2d360cb8d37c7cd67
stops <stopsilver212@gmail.com>
2022-02-16 20:56:43 +0900

    스택 수열

--- AlgorithmStudy/src/part4/week35/jje/Stack_1874.java
+++ AlgorithmStudy/src/part4/week35/jje/Stack_1874.java
@@ -1,7 +1,41 @@
 package part4.week35.jje;
 
-public class Stack_1874 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+public class Stack_1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int last = 0;
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (input > last) {
+                for (int j = last + 1; j <= input; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                last = input;
+            } else {
+                if (stack.peek() != input)   { //peek : 스택의 가장 위에 있는 항목을 반환
+                    System.out.println("NO");
+                    return;
+                }
+            }
+            stack.pop();
+            sb.append("-\n");
+        }
+        System.out.println(sb);
     }
 }
+
+/*
+1234   -> 4 ++++-
+123    -> 3 -
+1256    -> 6 ++-
+ */
