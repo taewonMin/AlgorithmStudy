c6ee9df66a735fe244beee8ffc302f9d2aaf9231
gilbeee <7mini96@naver.com>
2022-02-16 20:34:42 +0900

    괄호

--- AlgorithmStudy/src/part4/week35/bbg/Stack_9012.java
+++ AlgorithmStudy/src/part4/week35/bbg/Stack_9012.java
@@ -1,7 +1,43 @@
 package part4.week35.bbg;
 
-public class Stack_9012 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Stack;
 
+/**
+ * 괄호
+ * @author BBG
+ * @since 2022.02.16
+ * 12700KB 104ms 922B
+ */
+public class Stack_9012 {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	Stack<String> stack = new Stack<>();
+    	final String NO = "NO"; 
+    	final String YES = "YES"; 
+    	final String l = "(";
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 0 ; i < n ; i++) {
+    		String[] arr = br.readLine().split("");
+    		for(int j = 0 ; j < arr.length ; j++) {
+    			if(l.equals(arr[j])) {
+    				stack.push(l);
+    			} else if(!stack.empty()){
+					stack.pop();
+    			} else {
+    				stack.push(l);
+    				break;
+    			}
+    		}
+    		bw.write(stack.empty() ? YES : NO);
+    		bw.newLine();
+    		stack.clear();
+    	}
+    	
+    	bw.flush();
+    	br.close();
+    	bw.close();
     }
 }
