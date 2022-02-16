25be3709bd688e6679d7a5a2062717989375f363
Jieun Jeong <stopsilver@kaoni.com>
2022-02-16 16:48:47 +0900

    괄호

--- AlgorithmStudy/src/part4/week35/jje/Stack_9012.java
+++ AlgorithmStudy/src/part4/week35/jje/Stack_9012.java
@@ -1,7 +1,32 @@
 package part4.week35.jje;
 
-public class Stack_9012 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+public class Stack_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine();
+            Stack<String> stack = new Stack<>();
+            String result = "";
+            for (int j = 0; j < temp.length(); j++) {
+                if(temp.charAt(j) == '('){
+                    stack.push("(");
+                } else if (stack.isEmpty()){
+                    result = "NO";
+                } else {
+                    stack.pop();
+                }
+            }
+            if (result.equals("")){
+                result = stack.isEmpty() ? "YES" : "NO";
+            }
+            System.out.println(result);
+        }
     }
 }
