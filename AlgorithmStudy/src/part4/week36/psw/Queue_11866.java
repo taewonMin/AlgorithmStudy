04c602caaa466f03a36447b2b5bfbbbca71cf456
Ia7756 <gramian7756@gmail.com>
2022-02-17 16:14:57 +0900

    백준 / 큐 / 요세푸스 문제0

--- AlgorithmStudy/src/part4/week36/psw/Queue_11866.java
+++ AlgorithmStudy/src/part4/week36/psw/Queue_11866.java
@@ -3,9 +3,10 @@ package part4.week36.psw;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
+//	23820	156
 public class Queue_11866 {
     public static void main(String[] args) {
+        // 시간복잡도 2N으로 해결이 가능 할까? 몰겠는뎅
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), k = sc.nextInt();
         Queue<Integer> queue = new LinkedList<>();
@@ -13,17 +14,15 @@ public class Queue_11866 {
             queue.add(i);
         }
         StringBuffer sb = new StringBuffer("<");
-        int idx = 1;
-        while (!queue.isEmpty()){
-            if (idx == k){
-                sb.append(queue.poll()+", ");
-                idx=1;
-            }
 
-            queue.add(queue.poll());
-            idx++;
-            if (queue.size()==1) sb.append(queue.poll()+">");
+        while (queue.size() != 1){
+            for (int i = 0; i < k-1; i++) {
+                queue.add(queue.poll());
+            }
+            sb.append(queue.poll()+", ");
         }
+
+        sb.append(queue.poll()+">");
         System.out.println(sb);
     }
 }
