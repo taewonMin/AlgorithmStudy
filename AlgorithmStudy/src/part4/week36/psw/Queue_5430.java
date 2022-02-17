30cca5ddaa3b16f0f33d60c2aa63871b9ef2be61
Ia7756 <gramian7756@gmail.com>
2022-02-17 21:58:39 +0900

    백준 / 덱 / AC

--- AlgorithmStudy/src/part4/week36/psw/Queue_5430.java
+++ AlgorithmStudy/src/part4/week36/psw/Queue_5430.java
@@ -1,7 +1,65 @@
 package part4.week36.psw;
 
-public class Queue_5430 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+//95360	932
+public class Queue_5430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringBuffer sb = new StringBuffer();
+            String function = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            int[] numbers = new int[n];
+            String[] line = br.readLine().replace("[","").replace("]","").split(",");
+
+            for (int j = 0; j < n; j++) {
+                numbers[j] = Integer.parseInt(line[j]);
+            }
+
+            int startIdx = 0, endIdx = n-1;
+            boolean isUpsideDown = false;
+            String str = "[]";
+            for (int j = 0; j < function.length(); j++) {
+                if (function.charAt(j) == 'R'){
+                    isUpsideDown = !isUpsideDown;
+                }else {
+
+                    if (endIdx-startIdx == -1) {
+                        str = "error";
+                        break;
+                    }
+
+                    if (isUpsideDown){
+                        endIdx--;
+                    }else {
+                        startIdx++;
+                    }
+                }
+            }
+
+            if (endIdx-startIdx == -1){
+                sb.append(str);
+            }else {
+                sb.append("[");
+                if (isUpsideDown){
+                    for (int j = endIdx; j >= startIdx; j--) {
+                        sb.append(numbers[j]);
+                        if (j != startIdx) sb.append(",");
+                    }
+                }else {
+                    for (int j = startIdx; j <= endIdx ; j++) {
+                        sb.append(numbers[j]);
+                        if (j != endIdx) sb.append(",");
+                    }
+                }
+                sb.append("]");
+            }
+            System.out.println(sb);
+        }
     }
 }
