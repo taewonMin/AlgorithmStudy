4b554d6c73310882595637a9d519c17758f1e94b
smarto <mtw9411@naver.com>
2022-02-23 20:00:17 +0900

    카드2

--- AlgorithmStudy/src/part4/week36/mtw/Queue_2164.java
+++ AlgorithmStudy/src/part4/week36/mtw/Queue_2164.java
@@ -1,7 +1,33 @@
-package part4.week36.mtw;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 
+// 카드2
 public class Queue_2164 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+    	for(int i=1; i<=N; i++) {
+    		queue.offer(i);
+    	}
+        while(true){
+            int num = queue.poll();
+            if(queue.isEmpty()){
+                bw.write(num+"");
+                break;
+            }
+            queue.offer(queue.poll());
+        }
+    			
+    	br.close();
+    	bw.flush();
+    	bw.close();
     }
 }
