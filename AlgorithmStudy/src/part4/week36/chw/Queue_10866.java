b2e3127e64e733bd9674618639445e7bb93eba7f
user <user@DESKTOP-S0E3LBB>
2022-02-23 21:18:35 +0900

    36주차 커밋

--- AlgorithmStudy/src/part4/week36/chw/Queue_10866.java
+++ AlgorithmStudy/src/part4/week36/chw/Queue_10866.java
@@ -5,3 +5,33 @@ public class Queue_10866 {
 
     }
 }
+/*
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+deq = deque()
+for _ in range(N):
+    arr = sys.stdin.readline().split()
+    if arr[0] == 'push_back':
+        deq.append(arr[1])
+    elif arr[0] == 'push_front':
+        deq.appendleft(arr[1])
+    elif arr[0] == 'empty':
+        if not deq:
+            print(1)
+        else:
+            print(0)
+    elif arr[0] == 'size':
+        print(len(deq))
+    elif not deq:
+        print(-1)
+    elif arr[0] == 'pop_back':
+        print(deq.pop())
+    elif arr[0] == 'pop_front':
+        print(deq.popleft())
+    elif arr[0] == 'back':
+        print(deq[-1])
+    elif arr[0] == 'front':
+        print(deq[0])
+*/
--- AlgorithmStudy/src/part4/week36/chw/Queue_11866.java
+++ AlgorithmStudy/src/part4/week36/chw/Queue_11866.java
@@ -5,3 +5,24 @@ public class Queue_11866 {
 
     }
 }
+/*
+
+import sys
+from collections import deque
+
+deq = deque()
+N, K = list(map(int, sys.stdin.readline().split()))
+answer = "<"
+for num in range(N):
+    deq.append(num+1)
+idx = 1
+while deq:
+    if idx % K == 0:
+        val = deq.popleft()
+        answer += str(val) + ', '
+    else:
+        deq.append(deq.popleft())
+    idx += 1
+print(answer[:-2] + '>')
+
+*/
