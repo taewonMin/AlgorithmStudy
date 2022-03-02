5b7da726855e358ccdbdc23324294175447753fd
smarto <mtw9411@naver.com>
2022-03-02 20:31:45 +0900

    쿼드트리

--- AlgorithmStudy/src/part4/week37/mtw/DivideConquer_1992.java
+++ AlgorithmStudy/src/part4/week37/mtw/DivideConquer_1992.java
@@ -1,7 +1,50 @@
 package part4.week37.mtw;
 
-public class DivideConquer_1992 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+// 쿼드트리
+public class DivideConquer_1992 {
+    static int[][] tree;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        tree = new int[N][N];
+        for(int i=0; i<N; i++){
+            tree[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        devideConquer(0, 0, N);
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+
+    static void devideConquer(int startX, int startY, int len){
+        int color = tree[startX][startY];
+        if(len==1){
+            sb.append(color);
+            return;
+        }
+        for(int i=startX; i<startX+len; i++){
+            for(int j=startY; j<startY+len; j++){
+                if(color != tree[i][j]){
+                    int half = len/2;
+                    sb.append("(");
+                    devideConquer(startX, startY, half);
+                    devideConquer(startX, startY+half, half);
+                    devideConquer(startX+half, startY, half);
+                    devideConquer(startX+half, startY+half, half);
+                    sb.append(")");
+                    return;
+                }
+            }
+        }
+        sb.append(color);
+        return;
     }
 }
