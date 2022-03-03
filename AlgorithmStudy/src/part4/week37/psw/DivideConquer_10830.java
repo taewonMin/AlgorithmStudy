cd4c06b1feb430ee74d7c42c282a005d580b7433
Ia7756 <gramian7756@gmail.com>
2022-03-03 10:37:08 +0900

    백준 / 분할정복 / 행렬 제곱

--- AlgorithmStudy/src/part4/week37/psw/DivideConquer_10830.java
+++ AlgorithmStudy/src/part4/week37/psw/DivideConquer_10830.java
@@ -1,38 +1,51 @@
 package part4.week37.psw;
 
 import java.util.Scanner;
-
+//	12924	116
 public class DivideConquer_10830 {
-    static int[][] line, pow;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), b = sc.nextInt();
-        line = new int[n][n];
-        pow = new int[n][n];
+        int n = sc.nextInt();
+        long b = sc.nextLong();
+        int[][] line = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                line[i][j] = sc.nextInt();
-                pow[i][j] = line[i][j];
+                line[i][j] = sc.nextInt()%1000;
             }
         }
-        for (int k = 1; k < b; k++) {
-            int[][] temp = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    temp[i][j] = dc(i,j);
-                }
+        int[][] map = dc(line,b);
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(map[i][j]+" ");
             }
-            pow=temp;
+            sb.append("\n");
         }
-        System.out.println();
+        System.out.println(sb);
     }
 
-    private static int dc(int i, int j) {
-        int sum = 0;
-        for (int k = 0; k < line.length; k++) {
-            sum += line[i][k]*pow[k][j];
+    private static int[][] dc(int[][] line, long b) {
+        if (b==1) return line;
+        int[][] temp = dc(line,b/2);
+        if (b%2==1) return pow(pow(temp,temp),line);
+        return pow(temp,temp);
+    }
+
+    private static int[][] pow(int[][] a, int[][] b) {
+        int n = a.length;
+        int[][] c = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                for (int k = 0; k < n; k++) {
+                    sum += (a[i][k]*b[k][j])%1000;
+                }
+                c[i][j] = sum%1000;
+            }
         }
-        return sum;
+
+        return c;
     }
 }
