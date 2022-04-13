1fbd75b40fcad50bdb7aed5a47348de09702938f
Jieun <stopsilver@kaoni.com>
2022-04-13 20:53:45 +0900

    [우선순위큐] 최대 힙

--- AlgorithmStudy/src/part5/week41/jje/PQ_11279.java
+++ AlgorithmStudy/src/part5/week41/jje/PQ_11279.java
@@ -1,7 +1,27 @@
 package part5.week41.jje;
 
-public class PQ_11279 {
-    public static void main(String[] args)  {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
+public class PQ_11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        int x = 0;
+
+        for (int i = 0; i < n; i++) {
+            x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (maxQueue.isEmpty()) System.out.println(0);
+                else System.out.println(maxQueue.poll());
+
+            } else {
+                maxQueue.add(x);
+            }
+        }
     }
 }
