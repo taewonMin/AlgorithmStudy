74bc799bbbc440ecdcf3d192447cf468573f7354
Jieun <stopsilver@kaoni.com>
2022-05-18 20:57:13 +0900

    [누적합]수열

--- AlgorithmStudy/src/part5/week44/jje/PrefixSum_2559.java
+++ AlgorithmStudy/src/part5/week44/jje/PrefixSum_2559.java
@@ -1,4 +1,26 @@
 package part5.week44.jje;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class PrefixSum_2559 {
+    public static void main(String[] args) throws IOException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int k = input[1];
+        int[] num = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] prefixSum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + num[i - 1];
+        }
+        int max = 0;
+        for (int j = k; j < n; j++) {
+            int resultSum = prefixSum[j] - prefixSum[j - k];
+            if (resultSum > max) max = resultSum;
+        }
+        System.out.println(max);
+    }
 }
