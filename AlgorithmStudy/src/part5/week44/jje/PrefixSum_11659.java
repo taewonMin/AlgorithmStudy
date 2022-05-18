493fd96dc654de0d0411a14665c4f2aca9513cfd
Jieun <stopsilver@kaoni.com>
2022-05-18 20:56:41 +0900

    [누적합] 구간합구하기

--- AlgorithmStudy/src/part5/week44/jje/PrefixSum_11659.java
+++ AlgorithmStudy/src/part5/week44/jje/PrefixSum_11659.java
@@ -1,4 +1,24 @@
 package part5.week44.jje;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class PrefixSum_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int m = input[1];
+        int[] num = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] prefixSum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + num[i - 1];
+        }
+        for (int i = 0; i < m; i++) {
+            int[] index = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.println(prefixSum[index[1]] - prefixSum[index[0] - 1]);
+        }
+    }
 }
