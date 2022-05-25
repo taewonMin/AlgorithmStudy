c528848c95cc44461e72032c5b7f92c18161ef20
Jieun Jeong <stopsilver@kaoni.com>
2022-05-25 19:29:29 +0900

    [누적합] 구간 합 구하기 5

--- AlgorithmStudy/src/part5/week45/jje/PrefixSum_11660.java
+++ AlgorithmStudy/src/part5/week45/jje/PrefixSum_11660.java
@@ -1,4 +1,28 @@
 package part5.week45.jje;
 
+import java.util.Scanner;
+
 public class PrefixSum_11660 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] prefixSum = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int inputNum = sc.nextInt();
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + inputNum;
+            }
+        }
+
+        int x1; int y1; int x2; int y2;
+        for (int i = 1; i <= m; i++) {
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            x2 = sc.nextInt();
+            y2 = sc.nextInt();
+            System.out.println(prefixSum[x2][y2] - prefixSum[x2][y1 - 1] - prefixSum[x1 - 1][y2] + prefixSum[x1 - 1][y1 - 1]);
+        }
+    }
 }
