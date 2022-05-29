6c0eecc32f23bc3d4f7f69d3e352bb0cc07d4d32
Ia7756 <gramian7756@gmail.com>
2022-05-29 20:55:25 +0900

    숨바꼭질3

---
+++ AlgorithmStudy/src/part5/week46/psw/Dijkstra_13549.java
@@ -0,0 +1,44 @@
+package part5.week46.psw;
+
+import java.util.*;
+
+public class Dijkstra_13549 {
+
+    static class Node {
+        int idx , dist;
+        public Node(int idx, int dist) {
+            this.idx = idx;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt(), end = sc.nextInt();
+        int[] dp = new int[1000001];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        boolean[] visited = new boolean[100001];
+
+        PriorityQueue<Node> queue = new PriorityQueue<>((a1,a2) -> {return Integer.compare(a1.dist,a2.dist);});
+        queue.add(new Node(start,0));
+        dp[start] = 0;
+
+        while (!queue.isEmpty()){
+            Node poll = queue.poll();
+            visited[poll.idx] = true;
+            int[] idx = {poll.idx*2,poll.idx+1,poll.idx-1};
+
+            for (int i = 0; i < 3; i++) {
+                if (boundaryChk(idx[i]) && !visited[idx[i]] && dp[idx[i]] > dp[poll.idx] + (i==0?0:1)){
+                    queue.offer(new Node(idx[i], dp[idx[i]] = dp[poll.idx] + (i==0?0:1)));
+                }
+            }
+        }
+
+        System.out.println(dp[end]);
+    }
+
+    private static boolean boundaryChk(int idx) {
+        return idx > -1 && idx < 100001;
+    }
+}
