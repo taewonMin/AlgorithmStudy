94e2868c237b94150b4db35a4bece618cd130111
Jieun <stopsilver@kaoni.com>
2022-06-15 20:45:00 +0900

    [투포인터] 두 수의 합

--- AlgorithmStudy/src/part5/week47/jje/TwoPointer_3273.java
+++ AlgorithmStudy/src/part5/week47/jje/TwoPointer_3273.java
@@ -1,7 +1,32 @@
 package part5.week47.jje;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TwoPointer_3273 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers);
+        int x = sc.nextInt();
+        int sum = 0;
+        int result = 0;
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+            sum = numbers[start] + numbers[end];
+            if (sum == x) result++;
+            if (sum <= x) start++;
+            else end--;
+        }
+
+        System.out.println(result);
     }
 }
