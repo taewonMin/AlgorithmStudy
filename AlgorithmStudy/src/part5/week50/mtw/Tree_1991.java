60b23581043dc1e3d4270250ac6f833fac321071
smarto <mtw9411@naver.com>
2022-07-20 19:46:28 +0900

    백준 / 트리 / 트리 순회

--- AlgorithmStudy/src/part5/week50/mtw/Tree_1991.java
+++ AlgorithmStudy/src/part5/week50/mtw/Tree_1991.java
@@ -1,5 +1,54 @@
 package part5.week50.mtw;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+// 트리 순회
 public class Tree_1991 {
+    static Map<String, String[]> tree = new HashMap<>();
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        
+        for(int i=0; i<N; i++) {
+        	String node = sc.next();
+        	String[] arr = new String[2];
+        	arr[0] = sc.next();
+        	arr[1] = sc.next();
+        	tree.put(node, arr);
+        }
+        
+        preOrder("A");
+        System.out.println();
+        inOrder("A");
+        System.out.println();
+        postOrder("A");
+        
+        sc.close();
+    }
     
+    static void preOrder(String root) {
+    	if(root.equals(".")) return;
+    	
+    	System.out.print(root);
+    	preOrder(tree.get(root)[0]);
+    	preOrder(tree.get(root)[1]);
+    }
+    static void inOrder(String root) {
+    	if(root.equals(".")) return;
+    	
+    	inOrder(tree.get(root)[0]);
+    	System.out.print(root);
+    	inOrder(tree.get(root)[1]);
+    }
+    static void postOrder(String root) {
+    	if(root.equals(".")) return;
+    	
+    	postOrder(tree.get(root)[0]);
+    	postOrder(tree.get(root)[1]);
+    	System.out.print(root);
+    }
 }
