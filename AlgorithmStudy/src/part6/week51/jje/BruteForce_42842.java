fc30120caff8bf508e2d9d81d15a01efdd0bd28d
Jieun Jeong <stopsilver@kaoni.com>
2022-07-27 18:37:10 +0900

    [완전탐색] 카펫

--- AlgorithmStudy/src/part6/week51/jje/BruteForce_42842.java
+++ AlgorithmStudy/src/part6/week51/jje/BruteForce_42842.java
@@ -1,4 +1,28 @@
 package part6.week51.jje;
 
+import java.util.Scanner;
+
 public class BruteForce_42842 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int brown = sc.nextInt();
+        int yellow = sc.nextInt();
+        int width = 0;
+        int height = 0;
+        int total = brown + yellow;
+        int[] answer = new int[2];
+
+        for (int i = 1; i < brown / 2; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i * j) == total && (i - 2) * (j - 2) == yellow) {
+                    width = i;
+                    height = j;
+                    break;
+                }
+            }
+        }
+        answer = new int[]{width, height};
+        System.out.println(width + ", " + height);
+
+    }
 }
